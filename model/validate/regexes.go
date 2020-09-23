cc66ed3a81ab99a82950fa94818238f7bb3ed545
지농 <jinhong0719@naver.com>
2020-09-23 19:10:17 +0900

    [ADD] <model/validate/regexes.go> UUID 및 time 관련 정규표현식 상수 추가

--- model/validate/regexes.go
+++ model/validate/regexes.go
@@ -1,1 +1,11 @@
 package validate
+
+const (
+	adminUUIDRegexString = "^admin-\\d{12}"
+	studentUUIDRegexString = "^student-\\d{12}"
+	teacherUUIDRegexString = "^teacher-\\d{12}"
+	parentUUIDRegexString = "^parent-\\d{12}"
+	clubUUIDRegexString = "^club-\\d{12}"
+	recruitmentUUIDRegexString = "^recruitment-\\d{12}"
+	timRegexString = "\\d{4}-\\d{2}-\\d{2}"
+)
