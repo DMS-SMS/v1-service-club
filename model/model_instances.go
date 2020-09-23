c854ad93e8f88223e893667d7b2dba6bce174b44
지농 <jinhong0719@naver.com>
2020-09-23 19:01:30 +0900

    [ADD] <model/model_instances.go> 모든 모델에 대한 전역 인스턴스 객체 선언 추가

--- model/model_instances.go
+++ model/model_instances.go
@@ -1,1 +1,9 @@
 package model
+
+var (
+	ClubInstance = new(Club)
+	ClubInformInstance = new(ClubInform)
+	ClubMemberInstance = new(ClubMember)
+	ClubRecruitmentInstance = new(ClubRecruitment)
+	RecruitMemberInstance = new(RecruitMember)
+)
