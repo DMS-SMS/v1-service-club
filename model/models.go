9e9d6fb8d4a808ee7c30bb3fedf45464d63f5782
지농 <jinhong0719@naver.com>
2020-09-23 15:13:21 +0900

    [ADD] <model/models.go> Club & ClubInform 모델 필드 gorm, validate 태그 설정 추가

--- model/models.go
+++ model/models.go
@@ -1,18 +1,24 @@
 package model
 
+import (
+	"github.com/jinzhu/gorm"
+)
+
 type Club struct {
-	UUID       uuid
-	LeaderUUID leaderUUID
+	gorm.Model
+	UUID       uuid       `gorm:"PRIMARY_KEY;Type:char(17);UNIQUE;INDEX" validate:"uuid=club,len=17"`
+	LeaderUUID leaderUUID `gorm:"Type:char(20);NOT NULL;UNIQUE;" validate:"uuid=student,len=20"`
 }
 
 type ClubInform struct {
-	ClubUUID     clubUUID
-	Name         name
-	ClubConcept  clubConcept
-	Introduction introduction
-	Field        field
-	Location     location
-	Floor        floor
-	Link         link
-	LogoURI      logoURI
+	gorm.Model
+	ClubUUID     clubUUID     `gorm:"Type:char(17);NOT NULL;UNIQUE" validate:"uuid=club,len=17"`
+	Name         name         `gorm:"Type:varchar(30);NOT NULL;UNIQUE" validate:"min=1,max=30"`
+	ClubConcept  clubConcept  `gorm:"Type:varchar(40)" validate:"max=40"`
+	Introduction introduction `gorm:"Type:varchar(100)" validate:"max=150"`
+	Field        field        `gorm:"Type:varchar(20);NOT NULL" validate:"min=1,max=20"`
+	Location     location     `gorm:"Type:varchar(20);NOT NULL;UNIQUE" validate:"min=1,max=20"`
+	Floor        floor        `gorm:"Type:tinyint(1);NOT NULL" validate:"range=1~5"`
+	Link         link         `gorm:"Type:varchar(100)" validate:"max=1--"`
+	LogoURI      logoURI      `gorm:"Type:varchar(100);NOT NULL" validate:"min=1,max=100"`
 }
