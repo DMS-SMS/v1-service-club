6ccbe20e51162036a84455f48a66428bf779cfcd
지농 <jinhong0719@naver.com>
2020-09-23 19:11:57 +0900

    [ADD] <model/validate/regexes.go> 위에서 선언한 정규표현식 문자열을 이용하여 regexp 객체 추가

--- model/validate/regexes.go
+++ model/validate/regexes.go
@@ -1,5 +1,7 @@
 package validate
 
+import "regexp"
+
 const (
 	adminUUIDRegexString = "^admin-\\d{12}"
 	studentUUIDRegexString = "^student-\\d{12}"
@@ -7,5 +9,15 @@ const (
 	parentUUIDRegexString = "^parent-\\d{12}"
 	clubUUIDRegexString = "^club-\\d{12}"
 	recruitmentUUIDRegexString = "^recruitment-\\d{12}"
-	timRegexString = "\\d{4}-\\d{2}-\\d{2}"
+	timeRegexString = "\\d{4}-\\d{2}-\\d{2}"
+)
+
+var (
+	adminUUIDRegex = regexp.MustCompile(adminUUIDRegexString)
+	studentUUIDRegex = regexp.MustCompile(studentUUIDRegexString)
+	teacherUUIDRegex = regexp.MustCompile(teacherUUIDRegexString)
+	parentUUIDRegex = regexp.MustCompile(parentUUIDRegexString)
+	clubUUIDRegex = regexp.MustCompile(clubUUIDRegexString)
+	recruitmentUUIDRegex = regexp.MustCompile(recruitmentUUIDRegexString)
+	timeRegex = regexp.MustCompile(timeRegexString)
 )
