012a199185043347f46c7f96207f28e771c1475d
지농 <jinhong0719@naver.com>
2020-09-23 22:08:08 +0900

    [ADD] <model/model_method_hooks.go> 모델의 필드에 대한 유효한 값들 상수로 추가 선언

--- model/model_method_hooks.go
+++ model/model_method_hooks.go
@@ -2,6 +2,21 @@ package model
 
 import "club/model/validate"
 
+const (
+	emptyString = ""
+	emptyInt = 0
+
+	validClubUUID = "club-111111111111"
+	validRecruitmentUUID = "recruitment-111111111111"
+	validLeaderUUID = "student-111111111111"
+	validName = "DMS"
+	validField = "SW 개발"
+	validLocation = "2-2반 교실"
+	validFloor = 3
+	validLogoURI = "logos/club-111111111111"
+	validRecruitConcept = "디자인에 좋은 감각이 있는 새로운 1학년 부원을 모집합니다!"
+)
+
 func (c *Club) BeforeCreate() error {
 	return validate.DBValidator.Struct(c)
 }
