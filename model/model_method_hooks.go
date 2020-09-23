207d871e2703b8462419ee133bd5eeb5bf4a0e9c
지농 <jinhong0719@naver.com>
2020-09-23 19:29:14 +0900

    [ADD] <model/model_method_hooks.go> 모든 모델에 대한 BeforeCreate Hooks 선언

--- model/model_method_hooks.go
+++ model/model_method_hooks.go
@@ -1,1 +1,23 @@
 package model
+
+import "club/model/validate"
+
+func (c *Club) BeforeCreate() error {
+	return validate.DBValidator.Struct(c)
+}
+
+func (ci *ClubInform) BeforeCreate() error {
+	return validate.DBValidator.Struct(ci)
+}
+
+func (cm *ClubMember) BeforeCreate() error {
+	return validate.DBValidator.Struct(cm)
+}
+
+func (cr *ClubRecruitment) BeforeCreate() error {
+	return validate.DBValidator.Struct(cr)
+}
+
+func (rm *RecruitMember) BeforeCreate() error {
+	return validate.DBValidator.Struct(rm)
+}
