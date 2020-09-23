2c052fcbd9d76c0c78d91decb15917447eaf2048
지농 <jinhong0719@naver.com>
2020-09-23 19:13:51 +0900

    [ADD] <model/validate/validator_instance.go> validate 전역 객체 추가 및 UUID 검사 등록

--- model/validate/validator_instance.go
+++ model/validate/validator_instance.go
@@ -1,1 +1,32 @@
 package validate
+
+import (
+	"github.com/go-playground/validator/v10"
+	"log"
+)
+
+var DBValidator *validator.Validate
+
+func init() {
+	DBValidator = validator.New()
+
+	if err := DBValidator.RegisterValidation("uuid", isValidateUUID); err != nil { log.Fatal(err) } // 문자열 전용
+}
+
+func isValidateUUID(fl validator.FieldLevel) bool {
+	switch fl.Param() {
+	case "admin":
+		return adminUUIDRegex.MatchString(fl.Field().String())
+	case "student":
+		return studentUUIDRegex.MatchString(fl.Field().String())
+	case "teacher":
+		return teacherUUIDRegex.MatchString(fl.Field().String())
+	case "parent":
+		return parentUUIDRegex.MatchString(fl.Field().String())
+	case "club":
+		return clubUUIDRegex.MatchString(fl.Field().String())
+	case "recruitment":
+		return recruitmentUUIDRegex.MatchString(fl.Field().String())
+	}
+	return false
+}
