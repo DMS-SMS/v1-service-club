813cbfaf47a366743393aee70f9869685fd099a0
지농 <jinhong0719@naver.com>
2020-09-23 14:28:43 +0900

    [ADD] <model/models.go> CLubInform 테이블 관련 모델 구조체 정의 추가

--- model/models.go
+++ model/models.go
@@ -4,3 +4,15 @@ type Club struct {
 	UUID       uuid
 	LeaderUUID leaderUUID
 }
+
+type ClubInform struct {
+	ClubUUID     clubUUID
+	Name         name
+	ClubConcept  clubConcept
+	Introduction introduction
+	Field        field
+	Location     location
+	Floor        floor
+	Link         link
+	LogoURI      logoURI
+}
