ac9ae93f53572968daa75698794bd3e5b5e05edf
지농 <jinhong0719@naver.com>
2020-09-23 23:33:42 +0900

    [ADD] <db/accessor.go> Accessor 인터페이스 선언 및 Create 기능 관련 메서드 정의

--- db/accessor.go
+++ db/accessor.go
@@ -1,1 +1,13 @@
 package db
+
+import (
+	"club/model"
+)
+
+type Accessor interface {
+	CreateClub(club *model.Club) (resultClub *model.Club, err error)
+	CreateClubInform(inform *model.ClubInform) (resultInform *model.ClubInform, err error)
+	CreateClubMember(clubMember *model.ClubMember) (resultMember *model.ClubMember, err error)
+	CreateRecruitment(recruit *model.ClubRecruitment) (resultRecruit *model.ClubRecruitment, err error)
+	CreateRecruitMember(recruitMember *model.RecruitMember) (resultMember *model.RecruitMember, err error)
+}
