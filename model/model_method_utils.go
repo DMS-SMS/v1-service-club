c8692174569910557f218edde75ba0e9bf3701c4
지농 <jinhong0719@naver.com>
2020-09-23 21:48:28 +0900

    [ADD] <model/model_method_utils.go> 모든 모델 ContraintName & TableName 조회 메서드 추가

--- model/model_method_utils.go
+++ model/model_method_utils.go
@@ -38,3 +38,16 @@ func (ci *ClubInform)      ExceptGormModel() *ClubInform      { return exceptGor
 func (cm *ClubMember)      ExceptGormModel() *ClubMember      { return exceptGormModel(cm).(*ClubMember) }
 func (cr *ClubRecruitment) ExceptGormModel() *ClubRecruitment { return exceptGormModel(cr).(*ClubRecruitment) }
 func (rm *RecruitMember)   ExceptGormModel() *RecruitMember   { return exceptGormModel(rm).(*RecruitMember) }
+
+// XXXConstraintName 메서드 -> XXX PK의 Constraint Name 값 반환 메서드
+func (ci *ClubInform)      ClubUUIDConstraintName()        string { return "club_informs_club_uuid_clubs_uuid_foreign" }
+func (cm *ClubMember)      ClubUUIDConstraintName()        string { return "club_members_club_uuid_clubs_uuid_foreign" }
+func (cr *ClubRecruitment) ClubUUIDConstraintName()        string { return "club_recruitments_club_uuid_clubs_uuid_foreign" }
+func (rm *RecruitMember)   RecruitmentUUIDConstraintName() string { return "recruit_members_recruitment_uuid_club_recruitments_uuid_foreign" }
+
+// TableName 메서드 -> 리시버 변수에 해당되는 테이블의 이름 반환 메서드
+func (c *Club)             TableName() string { return "clubs" }
+func (ci *ClubInform)      TableName() string { return "club_informs" }
+func (cm *ClubMember)      TableName() string { return "club_members" }
+func (cr *ClubRecruitment) TableName() string { return "club_recruitments" }
+func (rm *RecruitMember)   TableName() string { return "recruit_members" }
