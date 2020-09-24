9adf8d9599db3ff8ea2b803bfb7abb0e392fa602
지농 <jinhong0719@naver.com>
2020-09-24 17:15:57 +0900

    [ADD] <db/access/default_read.go> 복수의 쿼리 결과문을 반환하는 Read 기능의 메서드 구현 완료

--- db/access/default_read.go
+++ db/access/default_read.go
@@ -2,6 +2,8 @@ package access
 
 import (
 	"club/model"
+	"github.com/jinzhu/gorm"
+	"time"
 )
 
 func (d *_default) GetClubWithClubUUID(clubUUID string) (club *model.Club, err error) {
@@ -22,6 +24,45 @@ func (d *_default) GetRecruitmentWithClubUUID(clubUUID string) (recruit *model.C
 	return
 }
 
+func (d *_default) GetClubInformsSortByUpdateTime(offset, limit int, field, name string) (clubInforms []*model.ClubInform, err error) {
+	selectedTX := d.tx.New()
+	if field != "" {
+		selectedTX = selectedTX.Where("field = ?", field)
+	}
+	if name != "" {
+		selectedTX = selectedTX.Where("name = ?", name)
+	}
+
+	clubInforms = make([]*model.ClubInform, limit)
+	err = selectedTX.Order("updated_at desc").Limit(limit).Offset(offset).Find(&clubInforms).Error
+
+	if len(clubInforms) == 0 && err == nil {
+		err = gorm.ErrRecordNotFound
+	}
+
+	return
+}
+
+func (d *_default) GetRecruitmentsSortByCreateTime(offset, limit int, field, name string) (recruits []*model.ClubRecruitment, err error) {
+	selectedTX := d.tx.New()
+	if field != "" {
+		selectedTX = selectedTX.Where("field = ?", field)
+	}
+	if name != "" {
+		selectedTX = selectedTX.Where("name = ?", name)
+	}
+
+	recruits = make([]*model.ClubRecruitment, limit)
+	selectedTX = selectedTX.Where("end_period >= ?", time.Now()).Or("end_period IS NULL")
+	err = selectedTX.Order("created_at desc").Limit(limit).Offset(offset).Find(&recruits).Error
+
+	if len(recruits) == 0 && err == nil {
+		err = gorm.ErrRecordNotFound
+	}
+
+	return
+}
+
 func (d *_default) GetClubInformWithClubUUID(clubUUID string) (inform *model.ClubInform, err error) {
 	inform = new(model.ClubInform)
 	err = d.tx.Where("club_uuid = ?", clubUUID).Find(inform).Error
@@ -34,3 +75,46 @@ func (d *_default) GetRecruitmentWithRecruitmentUUID(recruitUUID string) (recrui
 	return
 }
 
+func (d *_default) GetClubMembersWithClubUUID(clubUUID string) (members []*model.ClubMember, err error) {
+	members = make([]*model.ClubMember, 5, 5)
+	err = d.tx.Where("club_uuid = ?", clubUUID).Find(&members).Error
+
+	if len(members) == 0 && err == nil {
+		err = gorm.ErrRecordNotFound
+	}
+
+	return
+}
+
+func (d *_default) GetRecruitMembersWithRecruitmentUUID(recruitUUID string) (members []*model.RecruitMember, err error) {
+	members = make([]*model.RecruitMember, 5, 5)
+	err = d.tx.Where("recruitment_uuid = ?", recruitUUID).Find(&members).Error
+
+	if len(members) == 0 && err == nil {
+		err = gorm.ErrRecordNotFound
+	}
+
+	return
+}
+
+func (d *_default) GetAllClubInforms() (informs []*model.ClubInform, err error) {
+	informs = make([]*model.ClubInform, 10, 10)
+	err = d.tx.Find(&informs).Error
+
+	if len(informs) == 0 && err == nil {
+		err = gorm.ErrRecordNotFound
+	}
+
+	return
+}
+
+func (d *_default) GetAllRecruitments() (recruitments []*model.ClubRecruitment, err error) {
+	recruitments = make([]*model.ClubRecruitment, 10, 10)
+	err = d.tx.Where("end_period >= ?", time.Now()).Or("end_period IS NULL").Find(&recruitments).Error
+
+	if len(recruitments) == 0 && err == nil {
+		err = gorm.ErrRecordNotFound
+	}
+
+	return
+}
