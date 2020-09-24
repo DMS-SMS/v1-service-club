41abcde2cc1b8ac25a047c4e7238fbe39533f711
지농 <jinhong0719@naver.com>
2020-09-24 15:05:07 +0900

    [ADD] <db/access/mock.go> Create, Update, Delete, DCL 관련 모의 메서드 추가

--- db/access/mock.go
+++ db/access/mock.go
@@ -1,6 +1,10 @@
 package access
 
-import "github.com/stretchr/testify/mock"
+import (
+	"club/model"
+	"github.com/jinzhu/gorm"
+	"github.com/stretchr/testify/mock"
+)
 
 type _mock struct {
 	mock *mock.Mock
@@ -8,4 +12,73 @@ type _mock struct {
 
 func Mock(mock *mock.Mock) _mock {
 	return _mock{mock: mock}
+}
+
+func (m _mock) CreateClub(club *model.Club) (resultClub *model.Club, err error) {
+	args := m.mock.Called(club)
+	return args.Get(0).(*model.Club), args.Error(1)
+}
+
+func (m _mock) CreateClubInform(inform *model.ClubInform) (resultInform *model.ClubInform, err error) {
+	args := m.mock.Called(inform)
+	return args.Get(0).(*model.ClubInform), args.Error(1)
+}
+
+func (m _mock) CreateClubMember(clubMember *model.ClubMember) (resultMember *model.ClubMember, err error) {
+	args := m.mock.Called(clubMember)
+	return args.Get(0).(*model.ClubMember), args.Error(1)
+}
+
+func (m _mock) CreateRecruitment(recruit *model.ClubRecruitment) (resultRecruit *model.ClubRecruitment, err error) {
+	args := m.mock.Called(recruit)
+	return args.Get(0).(*model.ClubRecruitment), args.Error(1)
+}
+
+func (m _mock) CreateRecruitMember(recruitMember *model.RecruitMember) (resultMember *model.RecruitMember, err error) {
+	args := m.mock.Called(recruitMember)
+	return args.Get(0).(*model.RecruitMember), args.Error(1)
+}
+
+func (m _mock) ChangeClubLeader(clubUUID, newLeaderUUID string) error {
+	return m.mock.Called(clubUUID, newLeaderUUID).Error(0)
+}
+
+func (m _mock) ModifyClubInform(clubUUID string, revisionInform *model.ClubInform) error {
+	return m.mock.Called(clubUUID, revisionInform).Error(0)
+}
+
+func (m _mock) ModifyRecruitment(recruitUUID string, revisionRecruit *model.ClubRecruitment) error {
+	return m.mock.Called(recruitUUID, revisionRecruit).Error(0)
+}
+
+func (m _mock) DeleteClub(clubUUID string) error {
+	return m.mock.Called(clubUUID).Error(0)
+}
+
+func (m _mock) DeleteClubInform(clubUUID string) error {
+	return m.mock.Called(clubUUID).Error(0)
+}
+
+func (m _mock) DeleteClubMember(clubUUID, studentUUID string) error {
+	return m.mock.Called(clubUUID, studentUUID).Error(0)
+}
+
+func (m _mock) DeleteRecruitment(recruitUUID string) error {
+	return m.mock.Called(recruitUUID).Error(0)
+}
+
+func (m _mock) DeleteAllRecruitMember(recruitUUID string) error {
+	return m.mock.Called(recruitUUID).Error(0)
+}
+
+func (m _mock) BeginTx() {
+	m.mock.Called()
+}
+
+func (m _mock) Commit() *gorm.DB {
+	return m.mock.Called().Get(0).(*gorm.DB)
+}
+
+func (m _mock) Rollback() *gorm.DB {
+	return m.mock.Called().Get(0).(*gorm.DB)
 }
