3256ab4b4643e117295ff44d0fd2e80f8d678c61
지농 <jinhong0719@naver.com>
2020-09-24 17:13:32 +0900

    [ADD] <db/access/default_read.go> 단일 쿼리 결과문을 반환하는 Read 기능의 메서드 구현 완료

--- db/access/default_read.go
+++ db/access/default_read.go
@@ -1,1 +1,36 @@
 package access
+
+import (
+	"club/model"
+)
+
+func (d *_default) GetClubWithClubUUID(clubUUID string) (club *model.Club, err error) {
+	club = new(model.Club)
+	err = d.tx.Where("uuid = ?", clubUUID).Find(club).Error
+	return
+}
+
+func (d *_default) GetClubWithLeaderUUID(leaderUUID string) (club *model.Club, err error) {
+	club = new(model.Club)
+	err = d.tx.Where("leader_uuid = ?", leaderUUID).Find(club).Error
+	return
+}
+
+func (d *_default) GetRecruitmentWithClubUUID(clubUUID string) (recruit *model.ClubRecruitment, err error) {
+	recruit = new(model.ClubRecruitment)
+	err = d.tx.Where("club_uuid = ?", clubUUID).Find(recruit).Error
+	return
+}
+
+func (d *_default) GetClubInformWithClubUUID(clubUUID string) (inform *model.ClubInform, err error) {
+	inform = new(model.ClubInform)
+	err = d.tx.Where("club_uuid = ?", clubUUID).Find(inform).Error
+	return
+}
+
+func (d *_default) GetRecruitmentWithRecruitmentUUID(recruitUUID string) (recruit *model.ClubRecruitment, err error) {
+	recruit = new(model.ClubRecruitment)
+	err = d.tx.Where("uuid = ?", recruitUUID).Find(recruit).Error
+	return
+}
+
