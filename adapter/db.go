33ba362ed6499c601e8da73b3c5f88026ce7daae
지농 <jinhong0719@naver.com>
2020-09-24 11:14:44 +0900

    [ADD] <adapter/db.go> Consul에서 특정 Key로 KV 조회 후 DB 연결하는 외장 함수 추가

--- adapter/db.go
+++ adapter/db.go
@@ -1,11 +1,15 @@
 package adapter
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/go-playground/validator/v10"
+	"github.com/hashicorp/consul/api"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"os"
+	"strings"
 )
 
 type DBConfig struct {
@@ -14,6 +18,33 @@ type DBConfig struct {
 	Port 	int	   `json:"port" validate:"required"`
 	User    string `json:"user" validate:"required"`
 	DB		string `json:"db" validate:"required"`
+}
+
+func ConnectDBWithConsul(cli *api.Client, key string) (db *gorm.DB, conf DBConfig, err error) {
+	kv, _, err := cli.KV().Get(key, nil)
+	if err != nil {
+		err = errors.New(fmt.Sprintf("unable to get %s KV from consul, err: %v", key, err.Error()))
+		return
+	}
+
+	if err = json.Unmarshal(kv.Value, &conf); err != nil {
+		err = errors.New(fmt.Sprintf("error occurs while unmarshal KV value into struct, err: %v", err.Error()))
+		return
+	}
+
+	if err = validator.New().Struct(&conf); err != nil {
+		err = errors.New(fmt.Sprintf("invalid %s KV value, err: %v", key, err.Error()))
+		return
+	}
+
+	conf.Dialect = strings.ToLower(conf.Dialect)
+	switch conf.Dialect {
+	case "mysql":
+		db, err = connectToMysql(conf)
+	default:
+		err = errors.New(fmt.Sprintf("%s is not supported db in this service.", conf.Dialect))
+	}
+	return
 }
 
 func connectToMysql(conf DBConfig) (db *gorm.DB, err error) {
