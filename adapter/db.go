79adcfa11e523d15dcb7591a2f5486b1599707e7
지농 <jinhong0719@naver.com>
2020-09-24 11:12:27 +0900

    [ADD] <adapter/db.go> DB 설정 모음 구조체 정의 및 해당 구조체로 MySQL 연결 함수 추가

--- adapter/db.go
+++ adapter/db.go
@@ -1,1 +1,28 @@
 package adapter
+
+import (
+	"errors"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"os"
+)
+
+type DBConfig struct {
+	Dialect string `json:"dialect" validate:"required"`
+	Host    string `json:"host" validate:"required"`
+	Port 	int	   `json:"port" validate:"required"`
+	User    string `json:"user" validate:"required"`
+	DB		string `json:"db" validate:"required"`
+}
+
+func connectToMysql(conf DBConfig) (db *gorm.DB, err error) {
+	pwd := os.Getenv("DB_PASSWORD")
+	if pwd == "" {
+		err = errors.New("please set DB_PASSWORD environment variable")
+		return
+	}
+	args := fmt.Sprintf("%s:%s@(%s)/%s?charset=utf8&parseTime=True&loc=Local", conf.User, pwd, conf.Host, conf.DB)
+	db, err = gorm.Open(conf.Dialect, args)
+	return
+}
