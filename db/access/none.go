02d7c6d5e83c1223e0dbb52b2ed8884fbaef08f8
지농 <jinhong0719@naver.com>
2020-09-24 15:40:03 +0900

    [ADD] <db/access/none.go> None 객체 정의 및 Accessor 인터페이스 구현 완료

--- db/access/none.go
+++ db/access/none.go
@@ -1,1 +1,40 @@
 package access
+
+import (
+	"club/model"
+	"github.com/jinzhu/gorm"
+)
+
+type None struct {}
+
+func (n None) CreateClub(club *model.Club) (_ *model.Club, _ error) { return }
+func (n None) CreateClubInform(inform *model.ClubInform) (_ *model.ClubInform, _ error) { return }
+func (n None) CreateClubMember(clubMember *model.ClubMember) (_ *model.ClubMember, _ error) { return }
+func (n None) CreateRecruitment(recruit *model.ClubRecruitment) (_ *model.ClubRecruitment, _ error) { return }
+func (n None) CreateRecruitMember(recruitMember *model.RecruitMember) (_ *model.RecruitMember, _ error) { return }
+
+func (n None) GetClubWithClubUUID(clubUUID string) (_ *model.Club, _ error) { return }
+func (n None) GetClubWithLeaderUUID(leaderUUID string) (_ *model.Club, _ error) { return }
+func (n None) GetRecruitmentWithClubUUID(clubUUID string) (_ *model.ClubRecruitment, _ error) { return }
+func (n None) GetClubInformsSortByUpdateTime(offset, limit int, field, name string) (_ []*model.ClubInform, _ error) { return }
+func (n None) GetRecruitmentsSortByCreateTime(offset, limit int, field, name string) (_ []*model.ClubRecruitment, _ error) { return }
+func (n None) GetClubInformWithClubUUID(clubUUID string) (_ *model.ClubInform, _ error) { return }
+func (n None) GetRecruitmentWithRecruitmentUUID(recruitUUID string) (_ *model.ClubRecruitment, _ error) { return }
+func (n None) GetClubMembersWithClubUUID(clubUUID string) (_ []*model.ClubMember, _ error) { return }
+func (n None) GetRecruitMembersWithRecruitmentUUID(recruitUUID string) (_ []*model.RecruitMember, _ error) { return }
+func (n None) GetAllClubInforms() (_ []*model.ClubInform, _ error) { return }
+func (n None) GetAllRecruitments() (_ []*model.ClubRecruitment, _ error) { return }
+
+func (n None) ChangeClubLeader(clubUUID, newLeaderUUID string) (_ error) { return }
+func (n None) ModifyClubInform(clubUUID string, revisionInform *model.ClubInform) (_ error) { return }
+func (n None) ModifyRecruitment(recruitUUID string, revisionRecruit *model.ClubRecruitment) (_ error) { return }
+
+func (n None) DeleteClub(clubUUID string) (_ error) { return }
+func (n None) DeleteClubInform(clubUUID string) (_ error) { return }
+func (n None) DeleteClubMember(clubUUID, studentUUID string) (_ error) { return }
+func (n None) DeleteRecruitment(recruitUUID string) (_ error) { return }
+func (n None) DeleteAllRecruitMember(recruitUUID string) (_ error) { return }
+
+func (n None) BeginTx() { return }
+func (n None) Commit() (_ *gorm.DB) { return }
+func (n None) Rollback() (_ *gorm.DB) { return }
