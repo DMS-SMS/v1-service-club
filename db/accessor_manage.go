0abb351aeceea8588ee6e5cf551a466b413cdf9e
지농 <jinhong0719@naver.com>
2020-09-24 11:04:40 +0900

    [ADD] <db/accessor_manage.go> Accessor 구현체 TX 관리용 객체 정의 및 생성자 선언

--- db/accessor_manage.go
+++ db/accessor_manage.go
@@ -1,1 +1,33 @@
 package db
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+)
+
+type AccessorManage struct {
+	accessorType  reflect.Type
+	accessorValue reflect.Value
+}
+
+func NewAccessorManage(accessor Accessor) (manager AccessorManage, err error) {
+	if accessor == nil {
+		err = errors.New(fmt.Sprintf("nil parameter is not allowed"))
+		return
+	}
+
+	accessorType := reflect.TypeOf(accessor)
+	accessorValue := reflect.ValueOf(accessor)
+
+	if accessorType.Kind() == reflect.Ptr {
+		accessorType = accessorType.Elem()
+		accessorValue = accessorValue.Elem()
+	}
+
+	manager = AccessorManage{
+		accessorType:  accessorType,
+		accessorValue: accessorValue,
+	}
+	return
+}
