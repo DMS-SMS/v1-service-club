e82de515033eb8ca8aecae30cc898d623e549146
지농 <jinhong0719@naver.com>
2020-09-24 22:17:43 +0900

    [ADD] <db/test/init_for_test.go> Consul을 이용하여 DB 연결 & Accessor 관리자 객체 초기화

--- db/test/init_for_test.go
+++ db/test/init_for_test.go
@@ -1,1 +1,35 @@
 package test
+
+import (
+	"club/adapter"
+	"club/db"
+	"club/db/access"
+	"github.com/hashicorp/consul/api"
+	"log"
+	"sync"
+)
+
+func init() {
+	cli, err := api.NewClient(api.DefaultConfig())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbc, _, err := adapter.ConnectDBWithConsul(cli, "db/club/local_test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.Migrate(dbc)
+
+	manager, err = db.NewAccessorManage(access.Default(dbc))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		testGroup = sync.WaitGroup{}
+		testGroup.Add(numberOfTestFunc)
+		testGroup.Wait()
+		_ = dbc.Close()
+	}()
+}
