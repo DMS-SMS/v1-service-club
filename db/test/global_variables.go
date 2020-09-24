41e1143e8c8a5489ba07fab0eadcd38424ae6f94
지농 <jinhong0719@naver.com>
2020-09-24 23:28:28 +0900

    [ADD] <db/test/global_variables.go> ClubInform 모델 관련 제약조건 실패 에러 변수 추가

--- db/test/global_variables.go
+++ db/test/global_variables.go
@@ -2,6 +2,9 @@ package test
 
 import (
 	"club/db"
+	"club/model"
+	"club/tool/mysqlerr"
+	"strings"
 	"sync"
 )
 
@@ -10,4 +13,16 @@ var (
 	testGroup sync.WaitGroup
 )
 
-const numberOfTestFunc = 1
+const numberOfTestFunc = 2
+
+var (
+	clubInformClubUUIDFKConstraintFailError = mysqlerr.FKConstraintFailWithoutReferenceInform(mysqlerr.FKInform{
+		DBName:         strings.ToLower("SMS_Club_Test_DB"),
+		TableName:      model.ClubInformInstance.TableName(),
+		ConstraintName: model.ClubInformInstance.ClubUUIDConstraintName(),
+		AttrName:       model.ClubInformInstance.ClubUUID.KeyName(),
+	}, mysqlerr.RefInform{
+		TableName: model.ClubInstance.TableName(),
+		AttrName:  model.ClubInstance.UUID.KeyName(),
+	})
+)
