d5dbbb1a16b0c0a36d586ee853911383f25a4044
지농 <jinhong0719@naver.com>
2020-09-24 14:40:50 +0900

    [MODIFY] <model/validate/validator_instance.go> time 유효성 검사 함수 제거 및 등록 X

--- model/validate/validator_instance.go
+++ model/validate/validator_instance.go
@@ -13,7 +13,6 @@ func init() {
 	DBValidator = validator.New()
 
 	if err := DBValidator.RegisterValidation("uuid", isValidateUUID); err != nil { log.Fatal(err) } // 문자열 전용
-	if err := DBValidator.RegisterValidation("time", isTime);         err != nil { log.Fatal(err) } // 문자열 전용
 	if err := DBValidator.RegisterValidation("range", isWithinRange); err != nil { log.Fatal(err) } // 정수 전용
 }
 
@@ -33,19 +32,6 @@ func isValidateUUID(fl validator.FieldLevel) bool {
 		return recruitmentUUIDRegex.MatchString(fl.Field().String())
 	}
 	return false
-}
-
-func isTime(fl validator.FieldLevel) bool {
-	field := fl.Field().String()
-	if field == "" {
-		return true
-	}
-
-	if len(field) != 10 {
-		return false
-	}
-
-	return timeRegex.MatchString(fl.Field().String())
 }
 
 func isWithinRange(fl validator.FieldLevel) bool {
