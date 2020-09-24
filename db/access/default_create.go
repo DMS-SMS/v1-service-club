5b881f3786519e159ece9162b50b2a47ae7176cf
지농 <jinhong0719@naver.com>
2020-09-24 15:45:00 +0900

    [ADD] <db/access/default_create.go> Default 객체 Create 기능 관련 메서드 구현 완료

--- db/access/default_create.go
+++ db/access/default_create.go
@@ -1,1 +1,28 @@
 package access
+
+import "club/model"
+
+func (d *_default) CreateClub(club *model.Club) (*model.Club, error) {
+	result := d.tx.Create(club)
+	return result.Value.(*model.Club), result.Error
+}
+
+func (d *_default) CreateClubInform(inform *model.ClubInform) (*model.ClubInform, error) {
+	result := d.tx.Create(inform)
+	return result.Value.(*model.ClubInform), result.Error
+}
+
+func (d *_default) CreateClubMember(member *model.ClubMember) (*model.ClubMember, error) {
+	result := d.tx.Create(member)
+	return result.Value.(*model.ClubMember), result.Error
+}
+
+func (d *_default) CreateRecruitment(recruitment *model.ClubRecruitment) (*model.ClubRecruitment, error) {
+	result := d.tx.Create(recruitment)
+	return result.Value.(*model.ClubRecruitment), result.Error
+}
+
+func (d *_default) CreateRecruitMember(member *model.RecruitMember) (*model.RecruitMember, error) {
+	result := d.tx.Create(member)
+	return result.Value.(*model.RecruitMember), result.Error
+}
