3586dc179ba62e4f84e7a93f47fb48067f653bcf
지농 <jinhong0719@naver.com>
2020-09-25 17:14:13 +0900

    [ADD] <db/test/integration_read_test.go> GetClubWithClubUUID 메서드 통합 테스트 함수 추가

--- db/test/integration_read_test.go
+++ db/test/integration_read_test.go
@@ -1,1 +1,57 @@
 package test
+
+import (
+	"club/model"
+	"github.com/jinzhu/gorm"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"testing"
+)
+
+func Test_Accessor_GetClubWithClubUUID(t *testing.T) {
+	access, err := manager.BeginTx()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		access.Rollback()
+		testGroup.Done()
+	}()
+
+	for _, club := range []*model.Club{
+		{
+			UUID:       "club-111111111111",
+			LeaderUUID: "student-111111111111",
+		},
+	} {
+		if _, err := access.CreateClub(club); err != nil {
+			log.Fatal(err, club)
+		}
+	}
+
+	tests := []struct {
+		ClubUUID     string
+		ExpectResult *model.Club
+		ExpectError  error
+	} {
+		{
+			ClubUUID: "club-111111111111",
+			ExpectResult: &model.Club{
+				UUID:       "club-111111111111",
+				LeaderUUID: "student-111111111111",
+			},
+			ExpectError: nil,
+		}, {
+			ClubUUID:     "club-222222222222",
+			ExpectResult: &model.Club{},
+			ExpectError:  gorm.ErrRecordNotFound,
+		},
+	}
+
+	for _, test := range tests {
+		result, err := access.GetClubWithClubUUID(test.ClubUUID)
+
+		assert.Equalf(t, test.ExpectError, err, "error assertion error (test case: %v)", test)
+		assert.Equalf(t, test.ExpectResult, result.ExceptGormModel(), "result club assertion error (test case: %v)", test)
+	}
+}
