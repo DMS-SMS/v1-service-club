b3d8c449cec5c478fcd5f0de42fa1890eb70472c
지농 <jinhong0719@naver.com>
2020-09-25 10:00:26 +0900

    [ADD] <db/test/global_variables.go> ClubMember 모델 FK Constraint Fail 에러 변수 추가

--- db/test/global_variables.go
+++ db/test/global_variables.go
@@ -13,7 +13,7 @@ var (
 	testGroup sync.WaitGroup
 )
 
-const numberOfTestFunc = 2
+const numberOfTestFunc = 3
 
 var (
 	clubInformClubUUIDFKConstraintFailError = mysqlerr.FKConstraintFailWithoutReferenceInform(mysqlerr.FKInform{
@@ -21,6 +21,16 @@ var (
 		TableName:      model.ClubInformInstance.TableName(),
 		ConstraintName: model.ClubInformInstance.ClubUUIDConstraintName(),
 		AttrName:       model.ClubInformInstance.ClubUUID.KeyName(),
+	}, mysqlerr.RefInform{
+		TableName: model.ClubInstance.TableName(),
+		AttrName:  model.ClubInstance.UUID.KeyName(),
+	})
+
+	clubMemberClubUUIDFKConstraintFailError = mysqlerr.FKConstraintFailWithoutReferenceInform(mysqlerr.FKInform{
+		DBName:         strings.ToLower("SMS_Club_Test_DB"),
+		TableName:      model.ClubMemberInstance.TableName(),
+		ConstraintName: model.ClubMemberInstance.ClubUUIDConstraintName(),
+		AttrName:       model.ClubMemberInstance.ClubUUID.KeyName(),
 	}, mysqlerr.RefInform{
 		TableName: model.ClubInstance.TableName(),
 		AttrName:  model.ClubInstance.UUID.KeyName(),
