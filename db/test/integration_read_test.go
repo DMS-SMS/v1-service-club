84349232ebbed5cb7f28b39d86205a7243d4a84e
지농 <jinhong0719@naver.com>
2020-09-25 22:50:37 +0900

    [ADD] <db/test/..._read_test.go> GetClubInformsSortByUpdateTime 통합 테스트 함수 추가

--- db/test/integration_read_test.go
+++ db/test/integration_read_test.go
@@ -203,3 +203,173 @@ func Test_Accessor_GetCurrentRecruitmentWithClubUUID(t *testing.T) {
 		assert.Equalf(t, test.ExpectResult, result.ExceptGormModel(), "result club assertion error (test case: %v)", test)
 	}
 }
+
+func Test_Accessor_GetClubInformsSortByUpdateTime(t *testing.T) {
+	access, err := manager.BeginTx()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		access.Rollback()
+		testGroup.Done()
+	}()
+
+	for _, club := range []*model.Club{
+		{
+			UUID:       "club-111111111111",
+			LeaderUUID: "student-111111111111",
+		}, {
+			UUID:       "club-222222222222",
+			LeaderUUID: "student-222222222222",
+		}, {
+			UUID:       "club-333333333333",
+			LeaderUUID: "student-333333333333",
+		},
+	} {
+		if _, err := access.CreateClub(club); err != nil {
+			log.Fatal(err, club)
+		}
+	}
+
+	for _, inform := range []*model.ClubInform{
+		{
+			ClubUUID: "club-111111111111",
+			Name:     "DMS",
+			Field:    "SW 개발",
+			Location: "2-1반 교실",
+			Floor:    "3",
+			LogoURI:  "logo.com/club-111111111111",
+		}, {
+			ClubUUID: "club-222222222222",
+			Name:     "SMS",
+			Field:    "SW 개발",
+			Location: "2-2반 교실",
+			Floor:    "3",
+			LogoURI:  "logo.com/club-222222222222",
+		}, {
+			ClubUUID: "club-333333333333",
+			Name:     "PMS",
+			Field:    "SW 개발",
+			Location: "2-3반 교실",
+			Floor:    "3",
+			LogoURI:  "logo.com/club-333333333333",
+		},
+	} {
+		if _, err := access.CreateClubInform(inform); err != nil {
+			log.Fatal(err, inform)
+		}
+		time.Sleep(time.Millisecond * 500)
+	}
+
+	time.Sleep(time.Millisecond * 500)
+	if err, _ := access.ModifyClubInform("club-222222222222", &model.ClubInform{
+		ClubConcept:  "DMS의 소속부서 SMS 입니다!",
+		Introduction: "School Management System 서비스를 개발 및 운영합니다",
+		Link:         "facebook.com/DMS-SMS",
+	}); err != nil {
+		log.Fatal(err)
+	}
+
+	tests := []struct {
+		Offset, Limit int
+		Field, Name   string
+		ExpectResults []*model.ClubInform
+		ExpectError   error
+	} {
+		{
+			Offset: 0,
+			Limit:  10,
+			ExpectResults: []*model.ClubInform{
+				{
+					ClubUUID:     "club-222222222222",
+					Name:         "SMS",
+					ClubConcept:  "DMS의 소속부서 SMS 입니다!",
+					Introduction: "School Management System 서비스를 개발 및 운영합니다",
+					Link:         "facebook.com/DMS-SMS",
+					Field:        "SW 개발",
+					Location:     "2-2반 교실",
+					Floor:        "3",
+					LogoURI:      "logo.com/club-222222222222",
+				}, {
+					ClubUUID: "club-333333333333",
+					Name:     "PMS",
+					Field:    "SW 개발",
+					Location: "2-3반 교실",
+					Floor:    "3",
+					LogoURI:  "logo.com/club-333333333333",
+				}, {
+					ClubUUID: "club-111111111111",
+					Name:     "DMS",
+					Field:    "SW 개발",
+					Location: "2-1반 교실",
+					Floor:    "3",
+					LogoURI:  "logo.com/club-111111111111",
+				},
+			},
+			ExpectError: nil,
+		}, {
+			Offset:        10,
+			Limit:         10,
+			ExpectError:   gorm.ErrRecordNotFound,
+		}, {
+			Offset: 0,
+			Limit:  10,
+			Field:  "SW",
+			ExpectResults: []*model.ClubInform{
+				{
+					ClubUUID:     "club-222222222222",
+					Name:         "SMS",
+					ClubConcept:  "DMS의 소속부서 SMS 입니다!",
+					Introduction: "School Management System 서비스를 개발 및 운영합니다",
+					Link:         "facebook.com/DMS-SMS",
+					Field:        "SW 개발",
+					Location:     "2-2반 교실",
+					Floor:        "3",
+					LogoURI:      "logo.com/club-222222222222",
+				}, {
+					ClubUUID: "club-333333333333",
+					Name:     "PMS",
+					Field:    "SW 개발",
+					Location: "2-3반 교실",
+					Floor:    "3",
+					LogoURI:  "logo.com/club-333333333333",
+				}, {
+					ClubUUID: "club-111111111111",
+					Name:     "DMS",
+					Field:    "SW 개발",
+					Location: "2-1반 교실",
+					Floor:    "3",
+					LogoURI:  "logo.com/club-111111111111",
+				},
+			},
+			ExpectError: nil,
+		}, {
+			Offset: 0,
+			Limit:  10,
+			Name:  "D",
+			ExpectResults: []*model.ClubInform{
+				{
+					ClubUUID: "club-111111111111",
+					Name:     "DMS",
+					Field:    "SW 개발",
+					Location: "2-1반 교실",
+					Floor:    "3",
+					LogoURI:  "logo.com/club-111111111111",
+				},
+			},
+			ExpectError: nil,
+		},
+	}
+
+	for _, test := range tests {
+		informs, err := access.GetClubInformsSortByUpdateTime(test.Offset, test.Limit, test.Field, test.Name)
+
+		var exceptedInforms []*model.ClubInform
+		for _, inform := range informs {
+			exceptedInforms = append(exceptedInforms, inform.ExceptGormModel())
+		}
+
+		assert.Equalf(t, test.ExpectError, err, "error assertion error (test case: %v)", test)
+		assert.Equalf(t, test.ExpectResults, exceptedInforms, "result informs assertion error (test case: %v)", test)
+	}
+}
