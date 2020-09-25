59b43de0c6431e2739b00903e21b2fbe14d04193
지농 <jinhong0719@naver.com>
2020-09-25 16:17:05 +0900

    [MODIFY] <model/models.go> ClubInform 모델 Name, Location 필드 UNIQUE 조건 제거

--- model/models.go
+++ model/models.go
@@ -13,11 +13,11 @@ type Club struct {
 type ClubInform struct {
 	gorm.Model
 	ClubUUID     clubUUID     `gorm:"Type:char(17);NOT NULL;UNIQUE" validate:"uuid=club,len=17"`
-	Name         name         `gorm:"Type:varchar(30);NOT NULL;UNIQUE" validate:"min=1,max=30"`
+	Name         name         `gorm:"Type:varchar(30);NOT NULL" validate:"min=1,max=30"`
 	ClubConcept  clubConcept  `gorm:"Type:varchar(40)" validate:"max=40"`
 	Introduction introduction `gorm:"Type:varchar(100)" validate:"max=150"`
 	Field        field        `gorm:"Type:varchar(20);NOT NULL" validate:"min=1,max=20"`
-	Location     location     `gorm:"Type:varchar(20);NOT NULL;UNIQUE" validate:"min=1,max=20"`
+	Location     location     `gorm:"Type:varchar(20);NOT NULL" validate:"min=1,max=20"`
 	Floor        floor        `gorm:"Type:tinyint(1);NOT NULL" validate:"range=1~5"`
 	Link         link         `gorm:"Type:varchar(100)" validate:"max=100"`
 	LogoURI      logoURI      `gorm:"Type:varchar(100);NOT NULL" validate:"min=1,max=100"`
