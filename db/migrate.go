bb5291bacc099a1403487529b829a0a15d77d56f
지농 <jinhong0719@naver.com>
2020-09-26 14:54:07 +0900

    [MODIFY] <db/migrate.go> Gorm 패키지 변경에 따른 Migration 로직 수정

--- db/migrate.go
+++ db/migrate.go
@@ -2,37 +2,17 @@ package db
 
 import (
 	"club/model"
-	"github.com/jinzhu/gorm"
+	"gorm.io/gorm"
 )
 
-func Migrate(db *gorm.DB) {
-	db.LogMode(false)
+func Migrate(db *gorm.DB) error {
+	migrator := db.Migrator()
 
-	db.DropTableIfExists(&model.ClubMember{})
-	db.DropTableIfExists(&model.ClubInform{})
-	db.DropTableIfExists(&model.RecruitMember{})
-	db.DropTableIfExists(&model.ClubRecruitment{})
-	db.DropTableIfExists(&model.Club{})
+	_ = migrator.DropTable(&model.ClubMember{})
+	_ = migrator.DropTable(&model.ClubInform{})
+	_ = migrator.DropTable(&model.RecruitMember{})
+	_ = migrator.DropTable(&model.ClubRecruitment{})
+	_ = migrator.DropTable(&model.Club{})
 
-	if !db.HasTable(&model.Club{}) {
-		db.CreateTable(&model.Club{})
-	}
-	if !db.HasTable(&model.ClubInform{}) {
-		db.CreateTable(&model.ClubInform{})
-	}
-	if !db.HasTable(&model.ClubMember{}) {
-		db.CreateTable(&model.ClubMember{})
-	}
-	if !db.HasTable(&model.ClubRecruitment{}) {
-		db.CreateTable(&model.ClubRecruitment{})
-	}
-	if !db.HasTable(&model.RecruitMember{}) {
-		db.CreateTable(&model.RecruitMember{})
-	}
-
-	db.AutoMigrate(&model.Club{}, &model.ClubInform{}, &model.ClubMember{}, &model.ClubRecruitment{}, &model.RecruitMember{})
-	db.Model(&model.ClubInform{}).AddForeignKey("club_uuid", "clubs(uuid)", "RESTRICT", "RESTRICT")
-	db.Model(&model.ClubMember{}).AddForeignKey("club_uuid", "clubs(uuid)", "RESTRICT", "RESTRICT")
-	db.Model(&model.ClubRecruitment{}).AddForeignKey("club_uuid", "clubs(uuid)", "RESTRICT", "RESTRICT")
-	db.Model(&model.RecruitMember{}).AddForeignKey("recruitment_uuid", "club_recruitments(uuid)", "RESTRICT", "RESTRICT")
+	return db.AutoMigrate(&model.Club{}, &model.ClubInform{}, &model.ClubMember{}, &model.ClubRecruitment{}, &model.RecruitMember{})
 }
