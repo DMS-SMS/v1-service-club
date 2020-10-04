3984749d955b83f3fc48e4efa1fe6d4adad38d03
지농 <jinhong0719@naver.com>
2020-10-04 23:04:46 +0900

    [ADD] <handler/.../test_case_for_admin.go> CreateNewClub 테스트 주도 객체 정의 추가

--- handler/for_test/test_case_for_admin.go
+++ handler/for_test/test_case_for_admin.go
@@ -1,1 +1,20 @@
 package for_test
+
+type Method string
+type Returns []interface{}
+
+type CreateNewClubCase struct {
+	UUID              string
+	Name, LeaderUUID  string
+	MemberUUIDs       []string
+	Field, Location   string
+	Floor             uint32
+	Logo              []byte
+	ClubUUID          string
+	XRequestID        string
+	SpanContextString string
+	ExpectedMethods   map[Method]Returns
+	ExpectedStatus    uint32
+	ExpectedCode      int32
+	ExpectedClubUUID  string
+}
