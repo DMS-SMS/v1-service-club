18c9643e0a9dd1b6ec8da78be03f0b534124b49b
지농 <jinhong0719@naver.com>
2020-10-04 21:25:26 +0900

    [ADD] <handler/default.go> default 객체 정의 및 클로저를 이용한 생성자 선언 추가

--- handler/default.go
+++ handler/default.go
@@ -1,1 +1,27 @@
 package handler
+
+import (
+	"club/db"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/opentracing/opentracing-go"
+)
+
+type _default struct {
+	accessManage db.AccessorManage
+	tracer       opentracing.Tracer
+	awsSession   *session.Session
+}
+
+type FieldSetter func(*_default)
+
+func Default(setters ...FieldSetter) *_default {
+	return newDefault(setters...)
+}
+
+func newDefault(setters ...FieldSetter) (h *_default) {
+	h = new(_default)
+	for _, setter := range setters {
+		setter(h)
+	}
+	return
+}
