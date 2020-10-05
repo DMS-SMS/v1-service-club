2b9a4d24e3a8bf8aae3844167b558e7e3894b60f
지농 <jinhong0719@naver.com>
2020-10-05 10:07:38 +0900

    [ADD] <handler/for_test/test_case_for_admin.go> CreateNewClubCase 값 설정 메서드 추가

--- handler/for_test/test_case_for_admin.go
+++ handler/for_test/test_case_for_admin.go
@@ -4,11 +4,10 @@ type Method string
 type Returns []interface{}
 
 type CreateNewClubCase struct {
-	UUID              string
-	Name, LeaderUUID  string
+	UUID, LeaderUUID  string
+	Name, Field       string
 	MemberUUIDs       []string
-	Field, Location   string
-	Floor             uint32
+	Floor, Location   string
 	Logo              []byte
 	ClubUUID          string
 	XRequestID        string
@@ -17,4 +16,33 @@ type CreateNewClubCase struct {
 	ExpectedStatus    uint32
 	ExpectedCode      int32
 	ExpectedClubUUID  string
+}
+
+func (test *CreateNewClubCase) ChangeEmptyValueToValidValue() {
+	if test.UUID == EmptyString              { test.UUID = validAdminUUID }
+	if test.Name == EmptyString              { test.Name = validClubName }
+	if test.LeaderUUID == EmptyString        { test.LeaderUUID = validLeaderUUID }
+	if len(test.MemberUUIDs) == EmptyInt     { test.MemberUUIDs = validMemberUUIDs }
+	if test.Field == EmptyString             { test.Field = validField }
+	if test.Location == EmptyString          { test.Location = validLocation }
+	if test.Floor == EmptyString             { test.Floor = validFloor }
+	if string(test.Logo) == EmptyString      { test.Logo = validImageByteArr }
+	if test.ClubUUID == EmptyString          { test.ClubUUID = validClubUUID }
+	if test.XRequestID == EmptyString        { test.XRequestID = validXRequestID }
+	if test.SpanContextString == EmptyString { test.SpanContextString = validSpanContextString }
+}
+
+func (test *CreateNewClubCase) ChangeEmptyReplaceValueToEmptyValue() {
+	if test.UUID == EmptyReplaceValueForString                     { test.UUID = "" }
+	if test.Name == EmptyReplaceValueForString                     { test.Name = "" }
+	if test.LeaderUUID == EmptyReplaceValueForString               { test.LeaderUUID = "" }
+	if len(test.MemberUUIDs) == emptyReplaceValueForMemberUUIDsLen { test.MemberUUIDs = []string{} }
+	if test.Field == EmptyReplaceValueForString                    { test.Field = "" }
+	if test.Location == EmptyReplaceValueForString                 { test.Location = "" }
+	if test.Floor == EmptyReplaceValueForString                    { test.Floor = "" }
+	// logo empty case 테스트 필요
+	if string(test.Logo) == EmptyReplaceValueForString             { test.Logo = []byte{} }
+	if test.ClubUUID == EmptyReplaceValueForString                 { test.ClubUUID = "" }
+	if test.XRequestID == EmptyReplaceValueForString               { test.XRequestID = "" }
+	if test.SpanContextString == EmptyReplaceValueForString        { test.SpanContextString = "" }
 }
