81642db20b90f2964f69d2aa14d650188cbd3ed2
지농 <jinhong0719@naver.com>
2020-10-05 09:09:43 +0900

    [ADD] <handler/for_test/valid_values.go> DB Model에 대해 유효한 값을 가지는 상수 선언 추가

--- handler/for_test/valid_values.go
+++ handler/for_test/valid_values.go
@@ -1,1 +1,12 @@
 package for_test
+
+const (
+	validAdminUUID = "admin-111111111111"
+	validStudentUUID = "student-111111111111"
+	validClubUUID = "club-111111111111"
+
+	validClubName = "DMS"
+	validField = "SW 개발"
+	validLocation = "2-2반 교실"
+	validFloor = "3"
+)
