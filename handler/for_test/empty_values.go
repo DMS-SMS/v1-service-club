5d8524caee50181206b882e7959e6bf7d685c685
지농 <jinhong0719@naver.com>
2020-10-05 10:06:45 +0900

    [ADD] <handler/for_test/empty_values.go> Empty 값 대체 변수 추가 및 초기화 진행

--- handler/for_test/empty_values.go
+++ handler/for_test/empty_values.go
@@ -1,6 +1,22 @@
 package for_test
 
+import (
+	"club/tool/random"
+)
+
 const (
 	EmptyString = ""
-	EmptyUint32 = uint32(0)
+	EmptyInt = 0
 )
+
+var (
+	EmptyReplaceValueForString string
+	EmptyReplaceValueForMemberUUIDs []string
+	emptyReplaceValueForMemberUUIDsLen int
+)
+
+func init() {
+	EmptyReplaceValueForString = random.StringConsistOfIntWithLength(10)
+	emptyReplaceValueForMemberUUIDsLen = random.IntWithLengthWithoutZero(3)
+	EmptyReplaceValueForMemberUUIDs = make([]string, emptyReplaceValueForMemberUUIDsLen)
+}
