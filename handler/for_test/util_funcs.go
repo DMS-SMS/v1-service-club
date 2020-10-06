24c4bee5ed87824a59cab0903825ac257c1aaf85
지농 <jinhong0719@naver.com>
2020-10-06 13:27:57 +0900

    [ADD] <handler/for_test/util_funcs.go> 현재 시점을 기준으로 Gorm.Model 생성 후 반환 함수 추가

--- handler/for_test/util_funcs.go
+++ handler/for_test/util_funcs.go
@@ -1,1 +1,17 @@
-package for_test
+package test
+
+import (
+	"club/tool/random"
+	"gorm.io/gorm"
+	"time"
+)
+
+func createGormModelOnCurrentTime() gorm.Model {
+	currentTime := time.Now()
+	return gorm.Model{
+		ID:        uint(random.Int64WithLength(3)),
+		CreatedAt: currentTime,
+		UpdatedAt: currentTime,
+		DeletedAt: nil,
+	}
+}
