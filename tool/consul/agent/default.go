b1783e3106360d7acf0968adbf3596442d364d68
지농 <jinhong0719@naver.com>
2020-10-07 16:46:46 +0900

    [ADD] <tool/consul/agent.default.go> default 객체 일부 필드 추가 및 생성자 형식 변경

--- tool/consul/agent/default.go
+++ tool/consul/agent/default.go
@@ -1,18 +1,40 @@
 package agent
 
 import (
+	"github.com/hashicorp/consul/api"
 	"github.com/micro/go-micro/v2/client/selector"
 	"github.com/micro/go-micro/v2/registry"
 )
 
 type _default struct {
+	Client   api.Client
 	Strategy selector.Strategy
 	next     selector.Next
 	node     []*registry.Node
 }
 
-func Default(fn selector.Strategy) *_default {
-	return &_default{
-		Strategy: fn,
+func Default(setters ...FieldSetter) *_default {
+	return newDefault(setters...)
+}
+
+func newDefault(setters ...FieldSetter) (h *_default) {
+	h = new(_default)
+	for _, setter := range setters {
+		setter(h)
+	}
+	return
+}
+
+type FieldSetter func(*_default)
+
+func Client(c api.Client) FieldSetter {
+	return func(d *_default) {
+		d.Client = c
+	}
+}
+
+func Strategy(s selector.Strategy) FieldSetter {
+	return func(d *_default) {
+		d.Strategy = s
 	}
 }
