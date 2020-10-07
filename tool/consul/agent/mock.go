162493fb1d29915dc1fc6e31ee9eb6d574c57dd3
지농 <jinhong0719@naver.com>
2020-10-07 13:20:12 +0900

    [ADD] <tool/consul/agent/mock.go> mocking용 객체 정의 및 인터페이스 구현을 위한 메서드 선언

--- tool/consul/agent/mock.go
+++ tool/consul/agent/mock.go
@@ -1,1 +1,20 @@
 package agent
+
+import (
+	"github.com/micro/go-micro/v2/registry"
+	"github.com/stretchr/testify/mock"
+)
+
+type _mock struct {
+	mock *mock.Mock
+}
+
+func Mock(mock *mock.Mock) _mock {
+	return _mock{mock: mock}
+}
+
+
+func (m _mock) GetNextServiceNode() (registry.Node, error) {
+	args := m.mock.Called()
+	return args.Get(0).(registry.Node), args.Error(1)
+}
