fccb08ee1aeb18f8bc06a9a2f8c206033ee2b019
지농 <jinhong0719@naver.com>
2020-10-07 13:23:07 +0900

    [ADD] <tool/consul/agent/default.go> Default Consul Agent 객체 정의 및 생성자 추가

--- tool/consul/agent/default.go
+++ tool/consul/agent/default.go
@@ -1,1 +1,18 @@
 package agent
+
+import (
+	"github.com/micro/go-micro/v2/client/selector"
+	"github.com/micro/go-micro/v2/registry"
+)
+
+type _default struct {
+	Strategy selector.Strategy
+	next     selector.Next
+	node     []*registry.Node
+}
+
+func Default(fn selector.Strategy) *_default {
+	return &_default{
+		Strategy: fn,
+	}
+}
