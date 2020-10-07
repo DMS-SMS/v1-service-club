44e76152998ca6a487686b0b41d5cf7288060a2a
지농 <jinhong0719@naver.com>
2020-10-07 23:11:47 +0900

    [ADD] <handler/default.go> default handler 객체 일부 필드 추가 및 FieldSetter 함수 추가

--- handler/default.go
+++ handler/default.go
@@ -2,14 +2,19 @@ package handler
 
 import (
 	"club/db"
+	authproto "club/proto/golang/auth"
+	"club/tool/consul"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/opentracing/opentracing-go"
 )
 
 type _default struct {
+	None
 	accessManage db.AccessorManage
 	tracer       opentracing.Tracer
 	awsSession   *session.Session
+	consulAgent  consul.Agent
+	authStudent  authproto.AuthStudentService
 }
 
 type FieldSetter func(*_default)
@@ -41,5 +46,17 @@ func Tracer(t opentracing.Tracer) FieldSetter {
 func AWSSession(s *session.Session) FieldSetter {
 	return func(h *_default) {
 		h.awsSession = s
+	}
+}
+
+func ConsulAgent(ca consul.Agent) FieldSetter {
+	return func(h *_default) {
+		h.consulAgent = ca
+	}
+}
+
+func AuthStudent(as authproto.AuthStudentService) FieldSetter {
+	return func(h *_default) {
+		h.authStudent = as
 	}
 }
