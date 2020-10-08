0936dd6024e28ca2ef202d36391115112585d244
지농 <jinhong0719@naver.com>
2020-10-08 16:50:20 +0900

    [ADD] <handler/default_test_utils.go> 테스트 환경 설정 및 반환 함수 추가

--- handler/default_test_utils.go
+++ handler/default_test_utils.go
@@ -1,1 +1,34 @@
 package handler
+
+import (
+	"club/db"
+	"club/db/access"
+	authproto "club/proto/golang/auth"
+	consulagent "club/tool/consul/agent"
+	"fmt"
+	"github.com/stretchr/testify/mock"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	"log"
+)
+
+func setAndGetTestEnv() (newMock *mock.Mock, h *_default) {
+	newMock = new(mock.Mock)
+
+	exampleTracerForRPCService, closer, err := jaegercfg.Configuration{ServiceName: "DMS.SMS.v1.service.auth"}.NewTracer()
+	if err != nil { log.Fatal(fmt.Sprintf("error while creating new tracer for service, err: %v", err)) }
+	defer func() { _ = closer.Close() }()
+
+	mockAccessManage, err := db.NewAccessorManage(access.Mock(newMock))
+	if err != nil { log.Fatal(fmt.Sprintf("error while creating new access manage with mock, err: %v", err)) }
+
+	mockConsulAgent := consulagent.Mock(newMock)
+	mockAuthStudent := authproto.MockAuthStudentService(newMock)
+
+	h = Default(
+		AccessManager(mockAccessManage),
+		Tracer(exampleTracerForRPCService),
+		ConsulAgent(mockConsulAgent),
+		AuthStudent(mockAuthStudent),
+	)
+	return
+}
