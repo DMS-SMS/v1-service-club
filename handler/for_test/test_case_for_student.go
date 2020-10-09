0efc274995baf96a624d4ce5653bbc60679ff6bf
지농 <jinhong0719@naver.com>
2020-10-09 20:12:33 +0900

    [ADD] <.../test_case_for_student.go> GetClubsSortByUpdateTime 메서트 테스트 주도 객체 선언

--- handler/for_test/test_case_for_student.go
+++ handler/for_test/test_case_for_student.go
@@ -1,1 +1,13 @@
 package test
+
+type GetClubsSortByUpdateTimeCase struct {
+	UUID string
+	Field, Name string
+	Start, Count uint32
+	XRequestID        string
+	SpanContextString string
+	ExpectedMethods   map[Method]Returns
+	ExpectedStatus    uint32
+	ExpectedCode      int32
+	ExpectedClubUUID  string
+}
