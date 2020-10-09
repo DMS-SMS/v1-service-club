2ee1a3002d0d1e66b1c31eebc981479871403836
지농 <jinhong0719@naver.com>
2020-10-09 16:45:24 +0900

    [ADD] <tool/mysqlerr/parse_from.go> DuplicateEntry 에러 파싱 후 반환 함수 추가

--- tool/mysqlerr/parse_from.go
+++ tool/mysqlerr/parse_from.go
@@ -8,8 +8,36 @@ import (
 	"strings"
 )
 
-var regexForFKConstraintFail = regexp.MustCompile("`.*?`")
+var (
+	regexForDuplicateError = regexp.MustCompile("'.*?'")
+	regexForFKConstraintFail = regexp.MustCompile("`.*?`")
+)
 
+func ParseDuplicateEntryErrorFrom(mysqlErr *mysql.MySQLError) (key, entry string, err error) {
+	const (
+		indexEntry = iota
+		indexKey
+	)
+
+	if mysqlErr == nil || mysqlErr.Number != mysqlerr.ER_DUP_ENTRY {
+		err = errors.New("parameter must be Duplicate Entry Error")
+		return
+	}
+
+	matched := regexForDuplicateError.FindAllString(mysqlErr.Message, -1)
+	if len(matched) != 2 {
+		err = errors.New("this parameter is incorrect for the Duplicate Entry Error format")
+		return
+	}
+
+	for i := range matched {
+		matched[i] = strings.Trim(matched[i], "'")
+	}
+
+	key = matched[indexKey]
+	entry = matched[indexEntry]
+	return
+}
 
 func ParseFKConstraintFailErrorFrom(mysqlErr *mysql.MySQLError) (fk FKInform, ref RefInform, err error) {
 	const (
