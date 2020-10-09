033b228ddf52de77cf9ee738eee3d1a2e2521434
지농 <jinhong0719@naver.com>
2020-10-09 16:53:36 +0900

    [ADD] <handler/default_rpc_admin.go> CreateNewClub 메서드 선언 및 기본 환경 설정 코드 추가

--- handler/default_rpc_admin.go
+++ handler/default_rpc_admin.go
@@ -1,1 +1,36 @@
 package handler
+
+import (
+	clubproto "club/proto/golang/club"
+	code "club/utils/code/golang"
+	"context"
+	"fmt"
+	"github.com/uber/jaeger-client-go"
+	"net/http"
+)
+
+func (d *_default) CreateNewClub(ctx context.Context, req *clubproto.CreateNewClubRequest, resp *clubproto.CreateNewClubResponse) (_ error) {
+	ctx, proxyAuthenticated, reason := d.getContextFromMetadata(ctx)
+	if !proxyAuthenticated {
+		resp.Status = http.StatusProxyAuthRequired
+		resp.Message = fmt.Sprintf(proxyAuthRequiredMessageFormat, reason)
+		return
+	}
+
+	if !adminUUIDRegex.MatchString(req.UUID) {
+		resp.Status = http.StatusForbidden
+		resp.Message = fmt.Sprintf(forbiddenMessageFormat, "you are not admin")
+		return
+	}
+
+	reqID := ctx.Value("X-Request-Id").(string)
+	parentSpan := ctx.Value("Span-Context").(jaeger.SpanContext)
+
+	if !contains(req.MemberUUIDs, req.LeaderUUID) {
+		resp.Status = http.StatusConflict
+		resp.Code = code.MemberUUIDsNotIncludeLeaderUUID
+		resp.Message = fmt.Sprintf(conflictMessageFormat, "there is'nt leader uuid in member uuid list")
+		return
+	}
+
+}
