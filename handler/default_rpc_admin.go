bb584671916debde5a1789bdbb85b2e1fec54c3d
지농 <jinhong0719@naver.com>
2020-10-09 16:55:55 +0900

    [ADD] <handler/default_rpc_admin.go> GetStudentInformsWithUUIDs 호출 및 에러 처리 추가

--- handler/default_rpc_admin.go
+++ handler/default_rpc_admin.go
@@ -1,12 +1,15 @@
 package handler
 
 import (
+	authproto "club/proto/golang/auth"
 	clubproto "club/proto/golang/club"
 	consulagent "club/tool/consul/agent"
 	code "club/utils/code/golang"
 	topic "club/utils/topic/golang"
 	"context"
 	"fmt"
+	microerrors "github.com/micro/go-micro/v2/errors"
+	"github.com/micro/go-micro/v2/metadata"
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/log"
 	"github.com/uber/jaeger-client-go"
@@ -53,6 +56,60 @@ func (d *_default) CreateNewClub(ctx context.Context, req *clubproto.CreateNewCl
 	default:
 		resp.Status = http.StatusInternalServerError
 		resp.Message = fmt.Sprintf(internalServerMessageFormat, "unable to query in consul agent, err: " + err.Error())
+		return
+	}
+
+	spanForReq := d.tracer.StartSpan("GetStudentInformsWithUUIDs", opentracing.ChildOf(parentSpan))
+	md := metadata.Set(context.Background(), "X-Request-Id", reqID)
+	md = metadata.Set(md, "Span-Context", spanForReq.Context().(jaeger.SpanContext).String())
+	authReq := &authproto.GetStudentInformsWithUUIDsRequest{
+		UUID:         req.UUID,
+		StudentUUIDs: req.MemberUUIDs,
+	}
+	respOfReq, err := d.authStudent.GetStudentInformsWithUUIDs(md, authReq)
+	spanForReq.SetTag("X-Request-Id", reqID).LogFields(log.Object("request", authReq), log.Object("response", respOfReq), log.Error(err))
+	spanForReq.Finish()
+
+	// Handling Response Error of Request
+	switch assertedError := err.(type) {
+	case nil:
+		break
+	case *microerrors.Error:
+		switch assertedError.Code {
+		case http.StatusRequestTimeout:
+			resp.Status = http.StatusRequestTimeout
+			resp.Message = fmt.Sprintf(requestTimeoutMessageFormat, assertedError.Detail)
+			return
+		default:
+			resp.Status = http.StatusInternalServerError
+			resp.Message = fmt.Sprintf(internalServerMessageFormat, assertedError.Detail)
+			return
+		}
+	default:
+		resp.Status = http.StatusInternalServerError
+		resp.Message = fmt.Sprintf(internalServerMessageFormat, assertedError.Error())
+		return
+	}
+
+	// Handling Response Code of Request
+	switch respOfReq.Status {
+	case http.StatusOK:
+		break
+	case http.StatusConflict:
+		switch respOfReq.Code {
+		case code.StudentUUIDsContainNoExistUUID:
+			resp.Status = http.StatusConflict
+			resp.Code = code.MemberUUIDsIncludeNoExistUUID
+			resp.Message = fmt.Sprintf(conflictMessageFormat, "there is not exist uuid in member uuid list")
+			return
+		default:
+			resp.Status = http.StatusInternalServerError
+			resp.Message = fmt.Sprintf(internalServerMessageFormat, fmt.Sprintf("auth service returns unexpected code, code: %d", respOfReq.Code))
+			return
+		}
+	default:
+		resp.Status = respOfReq.Status
+		resp.Message = fmt.Sprintf("unexpected status returned, message: %s", respOfReq.Message)
 		return
 	}
 }
