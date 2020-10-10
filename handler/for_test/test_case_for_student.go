13d6bdb6ee3b79bc69eebcd312d994fddf777d90
지농 <jinhong0719@naver.com>
2020-10-10 22:33:15 +0900

    [ADD] <.../test_case_for_student.go> Create GetRecruitmentsSortByCreateTimeCase method

--- handler/for_test/test_case_for_student.go
+++ handler/for_test/test_case_for_student.go
@@ -95,3 +95,64 @@ type GetRecruitmentsSortByCreateTimeCase struct {
 	ExpectedCode       int32
 	ExpectRecruitments []*clubproto.RecruitmentInform
 }
+
+func (test *GetRecruitmentsSortByCreateTimeCase) ChangeEmptyValueToValidValue() {
+	if test.UUID == EmptyString              { test.UUID = validStudentUUID }
+	if test.XRequestID == EmptyString        { test.XRequestID = validXRequestID }
+	if test.SpanContextString == EmptyString { test.SpanContextString = validSpanContextString }
+}
+
+func (test *GetRecruitmentsSortByCreateTimeCase) ChangeEmptyReplaceValueToEmptyValue() {
+	if test.UUID == EmptyReplaceValueForString                     { test.UUID = "" }
+	if test.XRequestID == EmptyReplaceValueForString               { test.XRequestID = "" }
+	if test.SpanContextString == EmptyReplaceValueForString        { test.SpanContextString = "" }
+}
+
+func (test *GetRecruitmentsSortByCreateTimeCase) OnExpectMethodsTo(mock *mock.Mock) {
+	for method, returns := range test.ExpectedMethods {
+		test.onMethod(mock, method, returns)
+	}
+}
+
+func (test *GetRecruitmentsSortByCreateTimeCase) onMethod(mock *mock.Mock, method Method, returns Returns) {
+	switch method {
+	case "GetRecruitmentsSortByCreateTimeCase":
+		const defaultCountValue = 10
+		if test.Count == 0 {
+			test.Count = defaultCountValue
+		}
+		mock.On(string(method), int(test.Start), int(test.Count), test.Field, test.Name).Return(returns...)
+	case "GetRecruitMembersWithRecruitmentUUIDs":
+		const indexForRecruitments = 0
+		const indexForRecruitMembers = 0
+		const indexForError = 1
+		recruitments := test.ExpectedMethods["GetRecruitmentsSortByCreateTimeCase"][indexForRecruitments].([]*model.ClubRecruitment)
+		for index, recruitment := range recruitments {
+			mock.On(string(method), string(recruitment.UUID)).Return(returns[indexForRecruitMembers].([]*model.RecruitMember)[index], returns[indexForError])
+		}
+	case "BeginTx":
+		mock.On(string(method)).Return(returns...)
+	case "Commit":
+		mock.On(string(method)).Return(returns...)
+	case "Rollback":
+		mock.On(string(method)).Return(returns...)
+	default:
+		log.Fatalf("this method cannot be registered, method name: %s", method)
+	}
+}
+
+func (test *GetRecruitmentsSortByCreateTimeCase) SetRequestContextOf(req *clubproto.GetClubsSortByUpdateTimeRequest) {
+	req.UUID = test.UUID
+	req.Start = test.Start
+	req.Count = test.Count
+	req.Field = test.Field
+	req.Name = test.Name
+
+}
+
+func (test *GetRecruitmentsSortByCreateTimeCase) GetMetadataContext() (ctx context.Context) {
+	ctx = context.Background()
+	ctx = metadata.Set(ctx, "X-Request-Id", test.XRequestID)
+	ctx = metadata.Set(ctx, "Span-Context", test.SpanContextString)
+	return
+}
