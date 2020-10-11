ad3b753e049231213e029b7c73a7fffb234b92cb
지농 <jinhong0719@naver.com>
2020-10-11 11:55:42 +0900

    [ADD] <handler/default_rpc_student_test.go> Add Handling logic of test case

--- handler/default_rpc_student_test.go
+++ handler/default_rpc_student_test.go
@@ -559,4 +559,26 @@ func Test_Default_GetRecruitmentsSortByCreateTime(t *testing.T) {
 			ExpectedStatus: http.StatusInternalServerError,
 		},
 	}
+
+	for _, testCase := range tests {
+		newMock := &mock.Mock{}
+		handler := newDefaultMockHandler(newMock)
+
+		testCase.ChangeEmptyValueToValidValue()
+		testCase.ChangeEmptyReplaceValueToEmptyValue()
+		testCase.OnExpectMethodsTo(newMock)
+
+		req := new(clubproto.GetRecruitmentsSortByCreateTimeRequest)
+		testCase.SetRequestContextOf(req)
+		ctx := testCase.GetMetadataContext()
+
+		resp := new(clubproto.GetRecruitmentsSortByCreateTimeResponse)
+		_ = handler.GetRecruitmentsSortByCreateTime(ctx, req, resp)
+
+		assert.Equalf(t, int(testCase.ExpectedStatus), int(resp.Status), "status assertion error (test case: %v, message: %s)", testCase, resp.Message)
+		assert.Equalf(t, testCase.ExpectedCode, resp.Code, "code assertion error (test case: %v, message: %s)", testCase, resp.Message)
+		assert.Equalf(t, testCase.ExpectRecruitments, resp.Recruitments, "recruitments assertion error (test case: %v, message: %s)", testCase, resp.Message)
+
+		newMock.AssertExpectations(t)
+	}
 }
