fcd215c7c8edb48f464fa4d046254f164c165f8f
지농 <jinhong0719@naver.com>
2020-10-11 14:29:50 +0900

    [MODIFY] <handler/none.go> Modift method of None as changing of DB Accessor

--- handler/none.go
+++ handler/none.go
@@ -14,7 +14,6 @@ func (n None) DeleteClubMember(context.Context, *proto.DeleteClubMemberRequest, 
 func (n None) ChangeClubLeader(context.Context, *proto.ChangeClubLeaderRequest, *proto.ChangeClubLeaderResponse) (err error) { return }
 func (n None) ModifyClubInform(context.Context, *proto.ModifyClubInformRequest, *proto.ModifyClubInformResponse) (err error) { return }
 func (n None) DeleteClubWithUUID(context.Context, *proto.DeleteClubWithUUIDRequest, *proto.DeleteClubWithUUIDResponse) (err error) { return }
-func (n None) GetRecruitmentUUIDWithClubUUID(context.Context, *proto.GetRecruitmentUUIDWithClubUUIDRequest, *proto.GetRecruitmentUUIDWithClubUUIDResponse) (err error) { return }
 func (n None) RegisterRecruitment(context.Context, *proto.RegisterRecruitmentRequest, *proto.RegisterRecruitmentResponse) (err error) { return }
 func (n None) ModifyRecruitment(context.Context, *proto.ModifyRecruitmentRequest, *proto.ModifyRecruitmentResponse) (err error) { return }
 func (n None) DeleteRecruitmentWithUUID(context.Context, *proto.DeleteRecruitmentWithUUIDRequest, *proto.DeleteRecruitmentWithUUIDResponse) (err error) { return }
@@ -22,7 +21,10 @@ func (n None) DeleteRecruitmentWithUUID(context.Context, *proto.DeleteRecruitmen
 func (n None) GetClubsSortByUpdateTime(context.Context, *proto.GetClubsSortByUpdateTimeRequest, *proto.GetClubsSortByUpdateTimeResponse) (err error) { return }
 func (n None) GetRecruitmentsSortByCreateTime(context.Context, *proto.GetRecruitmentsSortByCreateTimeRequest, *proto.GetRecruitmentsSortByCreateTimeResponse) (err error) { return }
 func (n None) GetClubInformWithUUID(context.Context, *proto.GetClubInformWithUUIDRequest, *proto.GetClubInformWithUUIDResponse) (err error) { return }
+func (n None) GetClubInformsWithUUIDs(context.Context, *proto.GetClubInformsWithUUIDsRequest, *proto.GetClubInformsWithUUIDsResponse) (err error) { return }
 func (n None) GetRecruitmentInformWithUUID(context.Context, *proto.GetRecruitmentInformWithUUIDRequest, *proto.GetRecruitmentInformWithUUIDResponse) (err error) { return }
+func (n None) GetRecruitmentUUIDWithClubUUID(context.Context, *proto.GetRecruitmentUUIDWithClubUUIDRequest, *proto.GetRecruitmentUUIDWithClubUUIDResponse) (err error) { return }
+func (n None) GetRecruitmentUUIDsWithClubUUIDs(context.Context, *proto.GetRecruitmentUUIDsWithClubUUIDsRequest, *proto.GetRecruitmentUUIDsWithClubUUIDsResponse) (err error) { return }
 func (n None) GetAllClubFields(context.Context, *proto.GetAllClubFieldsRequest, *proto.GetAllClubFieldsResponse) (err error) { return }
 func (n None) GetTotalCountOfClubs(context.Context, *proto.GetTotalCountOfClubsRequest, *proto.GetTotalCountOfClubsResponse) (err error) { return }
 func (n None) GetTotalCountOfRecruitments(context.Context, *proto.GetTotalCountOfRecruitmentsRequest, *proto.GetTotalCountOfRecruitmentsResponse) (err error) { return }
