2dd1253dab3251bfd058d3469cfe5a14fe6334e2
지농 <jinhong0719@naver.com>
2020-10-11 11:16:33 +0900

    [ADD] <.../default_rpc_student_test.go> Add GetRecruitmentsSortByCreateTime test case

--- handler/default_rpc_student_test.go
+++ handler/default_rpc_student_test.go
@@ -10,6 +10,7 @@ import (
 	"gorm.io/gorm"
 	"net/http"
 	"testing"
+	"time"
 )
 
 func Test_Default_GetClubsSortByUpdateTime(t *testing.T) {
@@ -306,5 +307,291 @@ func Test_Default_GetClubsSortByUpdateTime(t *testing.T) {
 		assert.Equalf(t, testCase.ExpectClubInforms, resp.Informs, "club informs assertion error (test case: %v, message: %s)", testCase, resp.Message)
 
 		newMock.AssertExpectations(t)
+	}
+}
+
+func Test_Default_GetRecruitmentsSortByCreateTime(t *testing.T) {
+	now := time.Now()
+	startTime := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local)
+	endTime := startTime.Add(time.Hour * 24 * 7)
+
+	test := []*test.GetRecruitmentsSortByCreateTimeCase{
+		{ // success case
+			Start: 0,
+			Count: 10,
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx": {},
+				"GetRecruitmentsSortByCreateTimeCase": {[]*model.ClubRecruitment{{
+					UUID:           "recruitment-555555555555",
+					ClubUUID:       "club-333333333333",
+					RecruitConcept: "첫 번째 상시 채용",
+				}, {
+					UUID:           "recruitment-222222222222",
+					ClubUUID:       "club-111111111111",
+					RecruitConcept: "두 번째 공채",
+					StartPeriod:    model.StartPeriod(startTime),
+					EndPeriod:      model.EndPeriod(endTime),
+				}}, nil},
+				"GetRecruitMembersWithRecruitmentUUIDs": {[][]*model.RecruitMember{
+					{
+						{
+							RecruitmentUUID: "recruitment-555555555555",
+							Grade:           "1",
+							Field:           "서버 개발자",
+							Number:          "1",
+						}, {
+							RecruitmentUUID: "recruitment-555555555555",
+							Grade:           "1",
+							Field:           "웹 프론트 개발자",
+							Number:          "1",
+						},
+					}, {
+						{
+							RecruitmentUUID: "recruitment-222222222222",
+							Grade:           "1",
+							Field:           "모든 분야",
+							Number:          "4",
+						},
+					},
+				}, nil},
+				"Commit": {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusOK,
+			ExpectRecruitments: []*clubproto.RecruitmentInform{{
+				RecruitmentUUID: "recruitment-555555555555",
+				ClubUUID:        "club-333333333333",
+				RecruitConcept:  "첫 번째 상시 채용",
+				RecruitMembers: []*clubproto.RecruitMember{{
+					Grade:  "1",
+					Field:  "서버 개발자",
+					Number: "1",
+				}, {
+					Grade:  "1",
+					Field:  "웹 프론트 개발자",
+					Number: "1",
+				}},
+			}, {
+				RecruitmentUUID: "recruitment-222222222222",
+				ClubUUID:        "club-111111111111",
+				RecruitConcept:  "두 번째 공채",
+				RecruitMembers: []*clubproto.RecruitMember{{
+					Grade:  "1",
+					Field:  "모든 분야",
+					Number: "4",
+				}},
+				StartPeriod: startTime.Format("2006-01-02"),
+				EndPeriod:   endTime.Format("2006-01-02"),
+			}},
+		}, { // Start, Count Set X -> default(0, 10)
+			Start: 0,
+			Count: 10,
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx": {},
+				"GetRecruitmentsSortByCreateTimeCase": {[]*model.ClubRecruitment{{
+					UUID:           "recruitment-555555555555",
+					ClubUUID:       "club-333333333333",
+					RecruitConcept: "첫 번째 상시 채용",
+				}, {
+					UUID:           "recruitment-222222222222",
+					ClubUUID:       "club-111111111111",
+					RecruitConcept: "두 번째 공채",
+					StartPeriod:    model.StartPeriod(startTime),
+					EndPeriod:      model.EndPeriod(endTime),
+				}}, nil},
+				"GetRecruitMembersWithRecruitmentUUIDs": {[][]*model.RecruitMember{
+					{
+						{
+							RecruitmentUUID: "recruitment-555555555555",
+							Grade:           "1",
+							Field:           "서버 개발자",
+							Number:          "1",
+						}, {
+							RecruitmentUUID: "recruitment-555555555555",
+							Grade:           "1",
+							Field:           "웹 프론트 개발자",
+							Number:          "1",
+						},
+					}, {
+						{
+							RecruitmentUUID: "recruitment-222222222222",
+							Grade:           "1",
+							Field:           "모든 분야",
+							Number:          "4",
+						},
+					},
+				}, nil},
+				"Commit": {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusOK,
+			ExpectRecruitments: []*clubproto.RecruitmentInform{{
+				RecruitmentUUID: "recruitment-555555555555",
+				ClubUUID:        "club-333333333333",
+				RecruitConcept:  "첫 번째 상시 채용",
+				RecruitMembers: []*clubproto.RecruitMember{{
+					Grade:  "1",
+					Field:  "서버 개발자",
+					Number: "1",
+				}, {
+					Grade:  "1",
+					Field:  "웹 프론트 개발자",
+					Number: "1",
+				}},
+			}, {
+				RecruitmentUUID: "recruitment-222222222222",
+				ClubUUID:        "club-111111111111",
+				RecruitConcept:  "두 번째 공채",
+				RecruitMembers: []*clubproto.RecruitMember{{
+					Grade:  "1",
+					Field:  "모든 분야",
+					Number: "4",
+				}},
+				StartPeriod: startTime.Format("2006-01-02"),
+				EndPeriod:   endTime.Format("2006-01-02"),
+			}},
+		}, { // Set Name and Field
+			Name:  "SMS",
+			Field: "SW",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx": {},
+				"GetRecruitmentsSortByCreateTimeCase": {[]*model.ClubRecruitment{{
+					UUID:           "recruitment-555555555555",
+					ClubUUID:       "club-333333333333",
+					RecruitConcept: "첫 번째 상시 채용",
+				}}, nil},
+				"GetRecruitMembersWithRecruitmentUUIDs": {[][]*model.RecruitMember{
+					{
+						{
+							RecruitmentUUID: "recruitment-555555555555",
+							Grade:           "1",
+							Field:           "서버 개발자",
+							Number:          "1",
+						}, {
+							RecruitmentUUID: "recruitment-555555555555",
+							Grade:           "1",
+							Field:           "웹 프론트 개발자",
+							Number:          "1",
+						},
+					},
+				}, nil},
+				"Commit": {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusOK,
+			ExpectRecruitments: []*clubproto.RecruitmentInform{{
+				RecruitmentUUID: "recruitment-555555555555",
+				ClubUUID:        "club-333333333333",
+				RecruitConcept:  "첫 번째 상시 채용",
+				RecruitMembers: []*clubproto.RecruitMember{{
+					Grade:  "1",
+					Field:  "서버 개발자",
+					Number: "1",
+				}, {
+					Grade:  "1",
+					Field:  "웹 프론트 개발자",
+					Number: "1",
+				}},
+			}},
+		},  { // no exist X-Request-ID -> Proxy Authorization Required
+			XRequestID:      test.EmptyReplaceValueForString,
+			ExpectedMethods: map[test.Method]test.Returns{},
+			ExpectedStatus:  http.StatusProxyAuthRequired,
+		}, { // invalid X-Request-ID -> Proxy Authorization Required
+			XRequestID:      "InvalidXRequestID",
+			ExpectedMethods: map[test.Method]test.Returns{},
+			ExpectedStatus:  http.StatusProxyAuthRequired,
+		}, { // no exist Span-Context -> Proxy Authorization Required
+			SpanContextString: test.EmptyReplaceValueForString,
+			ExpectedMethods:   map[test.Method]test.Returns{},
+			ExpectedStatus:    http.StatusProxyAuthRequired,
+		}, { // invalid Span-Context -> Proxy Authorization Required
+			SpanContextString: "InvalidSpanContext",
+			ExpectedMethods:   map[test.Method]test.Returns{},
+			ExpectedStatus:    http.StatusProxyAuthRequired,
+		}, { // forbidden (not student)
+			UUID:           "parent-111111111112",
+			ExpectedStatus: http.StatusForbidden,
+		}, { // GetRecruitmentsSortByCreateTimeCase record not found
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx":                             {},
+				"GetRecruitmentsSortByCreateTimeCase": {[]*model.ClubRecruitment{}, gorm.ErrRecordNotFound},
+				"Commit":                              {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusOK,
+		}, { // GetRecruitmentsSortByCreateTimeCase returns unexpected error
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx":                             {},
+				"GetRecruitmentsSortByCreateTimeCase": {[]*model.ClubInform{}, errors.New("db connect fail")},
+				"Rollback":                            {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusInternalServerError,
+		}, { // GetRecruitMembersWithRecruitmentUUIDs returns RecordNotFound error
+			Name:  "SMS",
+			Field: "SW",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx": {},
+				"GetRecruitmentsSortByCreateTimeCase": {[]*model.ClubRecruitment{{
+					UUID:           "recruitment-555555555555",
+					ClubUUID:       "club-333333333333",
+					RecruitConcept: "첫 번째 상시 채용",
+				}}, nil},
+				"GetRecruitMembersWithRecruitmentUUIDs": {[][]*model.RecruitMember{{}}, gorm.ErrRecordNotFound},
+				"Commit":                                {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusOK,
+			ExpectRecruitments: []*clubproto.RecruitmentInform{{
+				RecruitmentUUID: "recruitment-555555555555",
+				ClubUUID:        "club-333333333333",
+				RecruitConcept:  "첫 번째 상시 채용",
+				RecruitMembers: []*clubproto.RecruitMember{},
+			}},
+		}, { // GetRecruitMembersWithRecruitmentUUIDs return unexpected error
+			Name:  "SMS",
+			Field: "SW",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx": {},
+				"GetRecruitmentsSortByCreateTimeCase": {[]*model.ClubRecruitment{{
+					UUID:           "recruitment-555555555555",
+					ClubUUID:       "club-333333333333",
+					RecruitConcept: "첫 번째 상시 채용",
+				}}, nil},
+				"GetRecruitMembersWithRecruitmentUUIDs": {[][]*model.RecruitMember{{}}, errors.New("unexpected error")},
+				"Rollback":                              {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusInternalServerError,
+		}, { // GetRecruitMembersWithRecruitmentUUIDs return abnormal length array
+			Name:  "SMS",
+			Field: "SW",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx": {},
+				"GetRecruitmentsSortByCreateTimeCase": {[]*model.ClubRecruitment{{
+					UUID:           "recruitment-555555555555",
+					ClubUUID:       "club-333333333333",
+					RecruitConcept: "첫 번째 상시 채용",
+				}}, nil},
+				"GetRecruitMembersWithRecruitmentUUIDs": {[][]*model.RecruitMember{
+					{
+						{
+							RecruitmentUUID: "recruitment-555555555555",
+							Grade:           "1",
+							Field:           "서버 개발자",
+							Number:          "1",
+						}, {
+							RecruitmentUUID: "recruitment-555555555555",
+							Grade:           "1",
+							Field:           "웹 프론트 개발자",
+							Number:          "1",
+						},
+					}, {
+						{
+							RecruitmentUUID: "recruitment-222222222222",
+							Grade:           "1",
+							Field:           "모든 분야",
+							Number:          "4",
+						},
+					},
+				}, nil},
+				"Rollback": {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusInternalServerError,
+		},
 	}
 }
