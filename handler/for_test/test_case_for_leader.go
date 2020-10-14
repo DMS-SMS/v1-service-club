1860492635dc6ce4eeccb45cdc4fdeb94b4727fa
지농 <jinhong0719@naver.com>
2020-10-14 16:05:43 +0900

    [ADD] <handler/for_test/test_case_for_leader.go> Add AddClubMemberCase

--- handler/for_test/test_case_for_leader.go
+++ handler/for_test/test_case_for_leader.go
@@ -1,1 +1,11 @@
 package test
+
+type AddClubMemberCase struct {
+	UUID                  string
+	ClubUUID, StudentUUID string
+	XRequestID            string
+	SpanContextString     string
+	ExpectedMethods       map[Method]Returns
+	ExpectedStatus        uint32
+	ExpectedCode          int32
+}
