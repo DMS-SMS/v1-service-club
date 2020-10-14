9d6100df64939b445e2ebbca8386917bb2f8d343
지농 <jinhong0719@naver.com>
2020-10-14 19:10:26 +0900

    [ADD] <handler/default_rpc_leader_test.go> Add test case for AddClubMember

--- handler/default_rpc_leader_test.go
+++ handler/default_rpc_leader_test.go
@@ -1,1 +1,349 @@
 package handler
+
+import (
+	test "club/handler/for_test"
+	"club/model"
+	authproto "club/proto/golang/auth"
+	consulagent "club/tool/consul/agent"
+	"club/tool/mysqlerr"
+	code "club/utils/code/golang"
+	microerrors "github.com/micro/go-micro/v2/errors"
+	"github.com/micro/go-micro/v2/registry"
+	"github.com/pkg/errors"
+	"gorm.io/gorm"
+	"net/http"
+	"testing"
+)
+
+func Test_Default_AddClubMember(t *testing.T) {
+	tests := []test.AddClubMemberCase{
+		{ // success case (student uuid)
+			UUID:        "student-111111111111",
+			ClubUUID:    "club-111111111111",
+			StudentUUID: "student-222222222222",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx": {},
+				"GetClubWithClubUUID": {&model.Club{
+					UUID:       "club-111111111111",
+					LeaderUUID: "student-111111111111",
+				}, nil},
+				"GetNextServiceNode": {&registry.Node{
+					Id:      "DMS.SMS.v1.service.auth-6b37b034-5f0b-4c9f-a03a-decbcb3799ef",
+					Address: "127.0.0.1:10101",
+				}},
+				"GetStudentInformWithUUID": {&authproto.GetStudentInformWithUUIDResponse{
+					Status:        http.StatusOK,
+					Message:       "get student inform success",
+					Grade:         2,
+					Group:         2,
+					StudentNumber: 7,
+					Name:          "박진홍",
+					PhoneNumber:   "01088378347",
+					ImageURI:      "profiles/student-111111111111",
+				}, nil},
+				"CreateClubMember": {&model.ClubMember{
+					ClubUUID:    "club-111111111111",
+					StudentUUID: "student-222222222222",
+				}, nil},
+				"Commit": {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusCreated,
+		}, { // success case (admin uuid)
+			UUID:        "admin-111111111111",
+			ClubUUID:    "club-111111111111",
+			StudentUUID: "student-222222222222",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx": {},
+				"GetClubWithClubUUID": {&model.Club{
+					UUID:       "club-111111111111",
+					LeaderUUID: "student-111111111111",
+				}, nil},
+				"GetNextServiceNode": {&registry.Node{
+					Id:      "DMS.SMS.v1.service.auth-6b37b034-5f0b-4c9f-a03a-decbcb3799ef",
+					Address: "127.0.0.1:10101",
+				}},
+				"GetStudentInformWithUUID": {&authproto.GetStudentInformWithUUIDResponse{
+					Status:        http.StatusOK,
+					Message:       "get student inform success",
+					Grade:         2,
+					Group:         2,
+					StudentNumber: 7,
+					Name:          "박진홍",
+					PhoneNumber:   "01088378347",
+					ImageURI:      "profiles/student-111111111111",
+				}, nil},
+				"CreateClubMember": {&model.ClubMember{
+					ClubUUID:    "club-111111111111",
+					StudentUUID: "student-222222222222",
+				}, nil},
+				"Commit": {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusCreated,
+		}, { // no exist X-Request-ID -> Proxy Authorization Required
+			XRequestID:      test.EmptyReplaceValueForString,
+			ExpectedMethods: map[test.Method]test.Returns{},
+			ExpectedStatus:  http.StatusProxyAuthRequired,
+		}, { // invalid X-Request-ID -> Proxy Authorization Required
+			XRequestID:      "InvalidXRequestID",
+			ExpectedMethods: map[test.Method]test.Returns{},
+			ExpectedStatus:  http.StatusProxyAuthRequired,
+		}, { // no exist Span-Context -> Proxy Authorization Required
+			SpanContextString: test.EmptyReplaceValueForString,
+			ExpectedMethods:   map[test.Method]test.Returns{},
+			ExpectedStatus:    http.StatusProxyAuthRequired,
+		}, { // invalid Span-Context -> Proxy Authorization Required
+			SpanContextString: "InvalidSpanContext",
+			ExpectedMethods:   map[test.Method]test.Returns{},
+			ExpectedStatus:    http.StatusProxyAuthRequired,
+		}, { // not student or admin uuid
+			UUID:            "parent-111111111111",
+			ExpectedMethods: map[test.Method]test.Returns{},
+			ExpectedStatus:  http.StatusForbidden,
+			ExpectedCode:    code.ForbiddenNotStudentOrAdminUUID,
+		}, { // not club leader
+			UUID:        "student-222222222222",
+			ClubUUID:    "club-111111111111",
+			StudentUUID: "student-333333333333",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx": {},
+				"GetClubWithClubUUID": {&model.Club{
+					UUID:       "club-111111111111",
+					LeaderUUID: "student-111111111111",
+				}, nil},
+				"Rollback": {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusForbidden,
+			ExpectedCode:   code.ForbiddenNotClubLeader,
+		}, { // GetClubWithClubUUID returns not exists
+			UUID:        "student-111111111111",
+			ClubUUID:    "club-111111111111",
+			StudentUUID: "student-222222222222",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx":             {},
+				"GetClubWithClubUUID": {&model.Club{}, gorm.ErrRecordNotFound},
+				"Rollback":            {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusNotFound,
+			ExpectedCode:   code.NotFoundClubNotExists,
+		}, { // GetClubWithClubUUID returns unexpected error
+			UUID:        "student-111111111111",
+			ClubUUID:    "club-111111111111",
+			StudentUUID: "student-222222222222",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx":             {},
+				"GetClubWithClubUUID": {&model.Club{}, errors.New("unexpected error")},
+				"Rollback":            {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusInternalServerError,
+		}, { // GetNextServiceNode return any error
+			UUID:        "student-111111111111",
+			ClubUUID:    "club-111111111111",
+			StudentUUID: "student-222222222222",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx": {},
+				"GetClubWithClubUUID": {&model.Club{
+					UUID:       "club-111111111111",
+					LeaderUUID: "student-111111111111",
+				}, nil},
+				"GetNextServiceNode": {&registry.Node{}, errors.New("I don't know what error is")},
+				"Rollback":           {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusInternalServerError,
+		}, { // GetNextServiceNode return ErrAvailableNodeNotFound
+			UUID:        "student-111111111111",
+			ClubUUID:    "club-111111111111",
+			StudentUUID: "student-222222222222",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx": {},
+				"GetClubWithClubUUID": {&model.Club{
+					UUID:       "club-111111111111",
+					LeaderUUID: "student-111111111111",
+				}, nil},
+				"GetNextServiceNode": {&registry.Node{}, consulagent.ErrAvailableNodeNotFound},
+				"Rollback":           {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusServiceUnavailable,
+		}, { // GetStudentInformWithUUID response 404
+			UUID:        "student-111111111111",
+			ClubUUID:    "club-111111111111",
+			StudentUUID: "student-222222222222",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx": {},
+				"GetClubWithClubUUID": {&model.Club{
+					UUID:       "club-111111111111",
+					LeaderUUID: "student-111111111111",
+				}, nil},
+				"GetNextServiceNode": {&registry.Node{
+					Id:      "DMS.SMS.v1.service.auth-6b37b034-5f0b-4c9f-a03a-decbcb3799ef",
+					Address: "127.0.0.1:10101",
+				}},
+				"GetStudentInformWithUUID": {&authproto.GetStudentInformWithUUIDResponse{
+					Status:  http.StatusNotFound,
+					Message: "student uuid not exist",
+				}, nil},
+				"Rollback": {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusNotFound,
+			ExpectedCode:   code.NotFoundStudentNotExist,
+		}, { // GetStudentInformWithUUID response not 200 or 404
+			UUID:        "student-111111111111",
+			ClubUUID:    "club-111111111111",
+			StudentUUID: "student-222222222222",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx": {},
+				"GetClubWithClubUUID": {&model.Club{
+					UUID:       "club-111111111111",
+					LeaderUUID: "student-111111111111",
+				}, nil},
+				"GetNextServiceNode": {&registry.Node{
+					Id:      "DMS.SMS.v1.service.auth-6b37b034-5f0b-4c9f-a03a-decbcb3799ef",
+					Address: "127.0.0.1:10101",
+				}},
+				"GetStudentInformWithUUID": {&authproto.GetStudentInformWithUUIDResponse{
+					Status:  http.StatusNetworkAuthenticationRequired,
+					Message: "I don't know about this error",
+				}, nil},
+				"Rollback": {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusNetworkAuthenticationRequired,
+		}, { // GetStudentInformWithUUID response not 200 or 404
+			UUID:        "student-111111111111",
+			ClubUUID:    "club-111111111111",
+			StudentUUID: "student-222222222222",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx": {},
+				"GetClubWithClubUUID": {&model.Club{
+					UUID:       "club-111111111111",
+					LeaderUUID: "student-111111111111",
+				}, nil},
+				"GetNextServiceNode": {&registry.Node{
+					Id:      "DMS.SMS.v1.service.auth-6b37b034-5f0b-4c9f-a03a-decbcb3799ef",
+					Address: "127.0.0.1:10101",
+				}},
+				"GetStudentInformWithUUID": {&authproto.GetStudentInformWithUUIDResponse{
+					Status:  http.StatusNetworkAuthenticationRequired,
+					Message: "I don't know about this error",
+				}, nil},
+				"Rollback": {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusNetworkAuthenticationRequired,
+		}, { // GetStudentInformWithUUID response timeout error
+			UUID:        "student-111111111111",
+			ClubUUID:    "club-111111111111",
+			StudentUUID: "student-222222222222",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx": {},
+				"GetClubWithClubUUID": {&model.Club{
+					UUID:       "club-111111111111",
+					LeaderUUID: "student-111111111111",
+				}, nil},
+				"GetNextServiceNode": {&registry.Node{
+					Id:      "DMS.SMS.v1.service.auth-6b37b034-5f0b-4c9f-a03a-decbcb3799ef",
+					Address: "127.0.0.1:10101",
+				}},
+				"GetStudentInformWithUUID": {&authproto.GetStudentInformWithUUIDResponse{}, &microerrors.Error{
+					Code:   http.StatusRequestTimeout,
+					Detail: "request time out",
+				}},
+				"Rollback": {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusRequestTimeout,
+		}, { // GetStudentInformWithUUID response unexpected error code
+			UUID:        "student-111111111111",
+			ClubUUID:    "club-111111111111",
+			StudentUUID: "student-222222222222",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx": {},
+				"GetClubWithClubUUID": {&model.Club{
+					UUID:       "club-111111111111",
+					LeaderUUID: "student-111111111111",
+				}, nil},
+				"GetNextServiceNode": {&registry.Node{
+					Id:      "DMS.SMS.v1.service.auth-6b37b034-5f0b-4c9f-a03a-decbcb3799ef",
+					Address: "127.0.0.1:10101",
+				}},
+				"GetStudentInformWithUUID": {&authproto.GetStudentInformWithUUIDResponse{}, &microerrors.Error{
+					Code:   http.StatusNetworkAuthenticationRequired,
+					Detail: "I don't know about this error",
+				}},
+				"Rollback": {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusInternalServerError,
+		}, { // GetStudentInformWithUUID response unexpected type of error
+			UUID:        "student-111111111111",
+			ClubUUID:    "club-111111111111",
+			StudentUUID: "student-222222222222",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx": {},
+				"GetClubWithClubUUID": {&model.Club{
+					UUID:       "club-111111111111",
+					LeaderUUID: "student-111111111111",
+				}, nil},
+				"GetNextServiceNode": {&registry.Node{
+					Id:      "DMS.SMS.v1.service.auth-6b37b034-5f0b-4c9f-a03a-decbcb3799ef",
+					Address: "127.0.0.1:10101",
+				}},
+				"GetStudentInformWithUUID": {&authproto.GetStudentInformWithUUIDResponse{}, errors.New("unexpected error")},
+				"Rollback":                 {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusInternalServerError,
+		}, { // CreateClubMember returns duplicate error
+			UUID:        "student-111111111111",
+			ClubUUID:    "club-111111111111",
+			StudentUUID: "student-222222222222",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx": {},
+				"GetClubWithClubUUID": {&model.Club{
+					UUID:       "club-111111111111",
+					LeaderUUID: "student-111111111111",
+				}, nil},
+				"GetNextServiceNode": {&registry.Node{
+					Id:      "DMS.SMS.v1.service.auth-6b37b034-5f0b-4c9f-a03a-decbcb3799ef",
+					Address: "127.0.0.1:10101",
+				}},
+				"GetStudentInformWithUUID": {&authproto.GetStudentInformWithUUIDResponse{
+					Status:        http.StatusOK,
+					Message:       "get student inform success",
+					Grade:         2,
+					Group:         2,
+					StudentNumber: 7,
+					Name:          "박진홍",
+					PhoneNumber:   "01088378347",
+					ImageURI:      "profiles/student-111111111111",
+				}, nil},
+				"CreateClubMember": {&model.ClubMember{}, mysqlerr.DuplicateEntry(model.ClubMemberInstance.StudentUUID.KeyName(), "student-222222222222")},
+				"Rollback":         {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusConflict,
+			ExpectedCode:   code.ThatUUIDAlreadyExistsAsMember,
+		}, { // CreateClubMember returns unexpected error
+			UUID:        "student-111111111111",
+			ClubUUID:    "club-111111111111",
+			StudentUUID: "student-222222222222",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx": {},
+				"GetClubWithClubUUID": {&model.Club{
+					UUID:       "club-111111111111",
+					LeaderUUID: "student-111111111111",
+				}, nil},
+				"GetNextServiceNode": {&registry.Node{
+					Id:      "DMS.SMS.v1.service.auth-6b37b034-5f0b-4c9f-a03a-decbcb3799ef",
+					Address: "127.0.0.1:10101",
+				}},
+				"GetStudentInformWithUUID": {&authproto.GetStudentInformWithUUIDResponse{
+					Status:        http.StatusOK,
+					Message:       "get student inform success",
+					Grade:         2,
+					Group:         2,
+					StudentNumber: 7,
+					Name:          "박진홍",
+					PhoneNumber:   "01088378347",
+					ImageURI:      "profiles/student-111111111111",
+				}, nil},
+				"CreateClubMember": {&model.ClubMember{}, errors.New("unexpected error")},
+				"Rollback":         {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusInternalServerError,
+		},
+	}
+}
