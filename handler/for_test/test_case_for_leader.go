d89402e1ab98ddfab28e7372ab100235cebfa31f
지농 <jinhong0719@naver.com>
2020-10-14 16:06:42 +0900

    [ADD] <handler/for_test/test_case_for_leader.go> Add mocking method in AddClubMemberCase

--- handler/for_test/test_case_for_leader.go
+++ handler/for_test/test_case_for_leader.go
@@ -1,5 +1,13 @@
 package test
 
+import (
+	"club/model"
+	authproto "club/proto/golang/auth"
+	topic "club/utils/topic/golang"
+	"github.com/stretchr/testify/mock"
+	"log"
+)
+
 type AddClubMemberCase struct {
 	UUID                  string
 	ClubUUID, StudentUUID string
@@ -9,3 +17,47 @@ type AddClubMemberCase struct {
 	ExpectedStatus        uint32
 	ExpectedCode          int32
 }
+func (test *AddClubMemberCase) OnExpectMethodsTo(mock *mock.Mock) {
+	for method, returns := range test.ExpectedMethods {
+		test.onMethod(mock, method, returns)
+	}
+}
+
+func (test *AddClubMemberCase) onMethod(mock *mock.Mock, method Method, returns Returns) {
+	switch method {
+	case "GetClubWithClubUUID":
+		mock.On(string(method), test.UUID).Return(returns...)
+	case "GetNextServiceNode":
+		mock.On(string(method), topic.AuthServiceName).Return(returns...)
+	case "GetStudentInformWithUUID":
+		mock.On(string(method), &authproto.GetStudentInformWithUUIDRequest{
+			UUID:        test.UUID,
+			StudentUUID: test.StudentUUID,
+		}).Return(returns...)
+	case "CreateClubMember":
+		const indexForClubMember = 0
+		const indexForError = 1
+		if _, ok := returns[indexForClubMember].(*model.ClubMember); ok && returns[indexForError] == nil {
+			memberForResp := test.getClubMember()
+			memberForResp.Model = createGormModelOnCurrentTime()
+			returns[indexForClubMember] = memberForResp
+		}
+		mock.On(string(method), test.getClubMember()).Return(returns...)
+	case "BeginTx":
+		mock.On(string(method)).Return(returns...)
+	case "Commit":
+		mock.On(string(method)).Return(returns...)
+	case "Rollback":
+		mock.On(string(method)).Return(returns...)
+	default:
+		log.Fatalf("this method cannot be registered, method name: %s", method)
+	}
+}
+
+func (test *AddClubMemberCase) getClubMember() *model.ClubMember {
+	return &model.ClubMember{
+		ClubUUID:    model.ClubUUID(test.ClubUUID),
+		StudentUUID: model.StudentUUID(test.StudentUUID),
+	}
+}
+
