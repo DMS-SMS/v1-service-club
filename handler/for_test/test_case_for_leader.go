ab21a83bebb99618a83d0b25b70590017bc7ed1a
지농 <jinhong0719@naver.com>
2020-10-14 22:24:45 +0900

    [ADD] <handler/for_test/test_case_for_leader.go> Add method of DeleteClubMemberCase

--- handler/for_test/test_case_for_leader.go
+++ handler/for_test/test_case_for_leader.go
@@ -88,7 +88,7 @@ func (test *AddClubMemberCase) GetMetadataContext() (ctx context.Context) {
 	return
 }
 
-type DeleteClubMember struct {
+type DeleteClubMemberCase struct {
 	UUID                  string
 	ClubUUID, StudentUUID string
 	XRequestID            string
@@ -96,4 +96,57 @@ type DeleteClubMember struct {
 	ExpectedMethods       map[Method]Returns
 	ExpectedStatus        uint32
 	ExpectedCode          int32
+}
+
+func (test *DeleteClubMemberCase) ChangeEmptyValueToValidValue() {
+	if test.XRequestID == EmptyString        { test.XRequestID = validXRequestID }
+	if test.SpanContextString == EmptyString { test.SpanContextString = validSpanContextString }
+}
+
+func (test *DeleteClubMemberCase) ChangeEmptyReplaceValueToEmptyValue() {
+	if test.XRequestID == EmptyReplaceValueForString        { test.XRequestID = "" }
+	if test.SpanContextString == EmptyReplaceValueForString { test.SpanContextString = "" }
+}
+
+func (test *DeleteClubMemberCase) OnExpectMethodsTo(mock *mock.Mock) {
+	for method, returns := range test.ExpectedMethods {
+		test.onMethod(mock, method, returns)
+	}
+}
+
+func (test *DeleteClubMemberCase) onMethod(mock *mock.Mock, method Method, returns Returns) {
+	switch method {
+	case "GetClubWithClubUUID":
+		mock.On(string(method), test.ClubUUID).Return(returns...)
+	case "DeleteClubMember":
+		mock.On(string(method), test.ClubUUID, test.StudentUUID).Return(returns...)
+	case "BeginTx":
+		mock.On(string(method)).Return(returns...)
+	case "Commit":
+		mock.On(string(method)).Return(returns...)
+	case "Rollback":
+		mock.On(string(method)).Return(returns...)
+	default:
+		log.Fatalf("this method cannot be registered, method name: %s", method)
+	}
+}
+
+func (test *DeleteClubMemberCase) getClubMember() *model.ClubMember {
+	return &model.ClubMember{
+		ClubUUID:    model.ClubUUID(test.ClubUUID),
+		StudentUUID: model.StudentUUID(test.StudentUUID),
+	}
+}
+
+func (test *DeleteClubMemberCase) SetRequestContextOf(req *clubproto.DeleteClubMemberRequest) {
+	req.UUID = test.UUID
+	req.ClubUUID = test.ClubUUID
+	req.StudentUUID = test.StudentUUID
+}
+
+func (test *DeleteClubMemberCase) GetMetadataContext() (ctx context.Context) {
+	ctx = context.Background()
+	ctx = metadata.Set(ctx, "X-Request-Id", test.XRequestID)
+	ctx = metadata.Set(ctx, "Span-Context", test.SpanContextString)
+	return
 }
