0c1d068059edade440dc5b7b8edea3f2a60cb52c
지농 <jinhong0719@naver.com>
2020-10-15 18:32:05 +0900

    [ADD] <handler/default_rpc_leader.go> Add logic for GetClubWithClubUUID method

--- handler/default_rpc_leader.go
+++ handler/default_rpc_leader.go
@@ -528,3 +528,58 @@ func (d *_default) ModifyClubInform(ctx context.Context, req *clubproto.ModifyCl
 	resp.Message = "success modify club inform"
 	return
 }
+
+func (d *_default) DeleteClubWithUUID(ctx context.Context, req *clubproto.DeleteClubWithUUIDRequest, resp *clubproto.DeleteClubWithUUIDResponse) (_ error) {
+	ctx, proxyAuthenticated, reason := d.getContextFromMetadata(ctx)
+	if !proxyAuthenticated {
+		resp.Status = http.StatusProxyAuthRequired
+		resp.Message = fmt.Sprintf(proxyAuthRequiredMessageFormat, reason)
+		return
+	}
+
+	switch true {
+	case adminUUIDRegex.MatchString(req.UUID):
+		break
+	case studentUUIDRegex.MatchString(req.UUID):
+		break
+	default:
+		resp.Status = http.StatusForbidden
+		resp.Code = code.ForbiddenNotStudentOrAdminUUID
+		resp.Message = fmt.Sprintf(forbiddenMessageFormat, "you are not student or admin")
+		return
+	}
+
+	reqID := ctx.Value("X-Request-Id").(string)
+	parentSpan := ctx.Value("Span-Context").(jaeger.SpanContext)
+
+	access := d.accessManage.BeginTx()
+
+	spanForDB := d.tracer.StartSpan("GetClubWithClubUUID", opentracing.ChildOf(parentSpan))
+	selectedClub, err := access.GetClubWithClubUUID(req.ClubUUID)
+	spanForDB.SetTag("X-Request-Id", reqID).LogFields(log.Object("SelectedClub", selectedClub), log.Error(err))
+	spanForDB.Finish()
+
+	switch err {
+	case nil:
+		break
+	case gorm.ErrRecordNotFound:
+		access.Rollback()
+		resp.Status = http.StatusNotFound
+		resp.Code = code.NotFoundClubNoExist
+		resp.Message = fmt.Sprintf(notFoundMessageFormat, "club with that uuid not exist")
+		return
+	default:
+		access.Rollback()
+		resp.Status = http.StatusInternalServerError
+		resp.Message = fmt.Sprintf(internalServerMessageFormat, "GetClubWithClubUUID returns unexpected error, err: " + err.Error())
+		return
+	}
+
+	if !adminUUIDRegex.MatchString(req.UUID) && req.UUID != string(selectedClub.LeaderUUID) {
+		access.Rollback()
+		resp.Status = http.StatusForbidden
+		resp.Code = code.ForbiddenNotClubLeader
+		resp.Message = fmt.Sprintf(forbiddenMessageFormat, "you're not admin and not club leader")
+		return
+	}
+}
