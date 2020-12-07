dca5e0ccc9de662a02e6669b5d520fbcfd8328e9
지농 <jinhong0719@naver.com>
2020-12-07 19:52:42 +0900

    [ADD] <handler/default_utils.go> Add teacherUUIDRegex

--- handler/default_utils.go
+++ handler/default_utils.go
@@ -21,6 +21,7 @@ const (
 var (
 	adminUUIDRegex = regexp.MustCompile("^admin-\\d{12}")
 	studentUUIDRegex = regexp.MustCompile("^student-\\d{12}")
+	teacherUUIDRegex = regexp.MustCompile("^teacher-\\d{12}")
 	clubUUIDRegex = regexp.MustCompile("^club-\\d{12}")
 )
 
