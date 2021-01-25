03fcedfe4301d49934e080c244a818f89d62a859
지농 <jinhong0719@naver.com>
2021-01-25 17:19:30 +0900

    [ADD] <main.go> Add starting subscriber & Change BeforeStart, AfterStart closure

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"club/adapter"
+	"club/consul"
+	consulagent "club/consul/agent"
 	"club/db"
 	"club/db/access"
 	"club/handler"
 	authproto "club/proto/golang/auth"
 	clubproto "club/proto/golang/club"
+	"club/subscriber"
 	"club/tool/closure"
-	consulagent "club/tool/consul/agent"
+	"club/tool/network"
 	topic "club/utils/topic/golang"
 	"fmt"
 	"github.com/InVisionApp/go-health/v2"
@@ -16,6 +18,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/hashicorp/consul/api"
 	"github.com/micro/go-micro/v2"
 	"github.com/micro/go-micro/v2/client"
@@ -26,37 +29,41 @@ import (
 	"github.com/uber/jaeger-client-go"
 	jaegercfg "github.com/uber/jaeger-client-go/config"
 	"log"
-	"math/rand"
-	"net"
 	"os"
 	"time"
 )
 
 func main() {
 	// create service
-	port := getRandomPortNotInUsedWithRange(10100, 10200)
+	port := network.GetRandomPortNotInUsedWithRange(10101, 10200)
 	service := micro.NewService(
 		micro.Name(topic.ClubServiceName),
-		micro.Version("1.0.3"),
+		micro.Version("1.0.5"),
 		micro.Transport(grpc.NewTransport()),
 		micro.Address(fmt.Sprintf(":%d", port)),
 	)
 	srvID := fmt.Sprintf("%s-%s", service.Server().Options().Name, service.Server().Options().Id)
 
-	// create consul connection
+	// create consul connection & agent
 	consulAddr := os.Getenv("CONSUL_ADDRESS")
 	if consulAddr == "" {
 		log.Fatal("please set CONSUL_ADDRESS in environment variable")
 	}
 	consulCfg := api.DefaultConfig()
 	consulCfg.Address = consulAddr
-	consul, err := api.NewClient(consulCfg)
+	consulCli, err := api.NewClient(consulCfg)
 	if err != nil {
 		log.Fatalf("consul connect fail, err: %v", err)
 	}
+	consulAgent := consulagent.Default( // add in v.1.0.5
+		consulagent.Strategy(selector.RoundRobin),
+		consulagent.Client(consulCli),
+		consulagent.Services([]consul.ServiceName{topic.AuthServiceName, topic.ClubServiceName,
+			topic.OutingServiceName, topic.ScheduleServiceName, topic.AnnouncementServiceName}),
+	)
 
 	// create db access manager
-	dbc, _, err := adapter.ConnectDBWithConsul(consul, "db/club/local")
+	dbc, _, err := db.ConnectWithConsul(consulCli, "db/club/local")
 	if err != nil {
 		log.Fatalf("db connect fail, err: %v", err)
 	}
@@ -107,29 +114,44 @@ func main() {
 		log.Fatalf("error while creating new aws session, err: %v", err)
 	}
 
-	// create rpc handler
-	defaultAgent := consulagent.Default(
-		consulagent.Strategy(selector.RoundRobin),
-		consulagent.Client(consul),
-	)
 	cliOpts := []client.Option{client.Transport(grpc.NewTransport())}
 	authStudentSrv := authproto.NewAuthStudentService(topic.AuthServiceName, grpccli.NewClient(cliOpts...))
-	rpcHandler := handler.Default(
+	defaultHandler := handler.Default(
 		handler.AccessManager(defaultAccessManage),
 		handler.Tracer(authSrvTracer),
-		handler.ConsulAgent(defaultAgent),
+		handler.ConsulAgent(consulAgent),
 		handler.AuthStudent(authStudentSrv),
 		handler.AWSSession(awsSession),
 	)
 
-	service.Init(
-		micro.AfterStart(closure.ConsulServiceRegistrar(service.Server(), consul)),
-		micro.BeforeStop(closure.ConsulServiceDeregistrar(service.Server(), consul)),
+	// create subscriber & register listener (add in v.1.0.5)
+	consulChangeQueue := os.Getenv("CHANGE_CONSUL_SQS_NAME")
+	if consulChangeQueue == "" {
+		log.Fatal("please set CHANGE_CONSUL_SQS_NAME in environment variable")
+	}
+	subscriber.SetAwsSession(awsSession)
+	defaultSubscriber := subscriber.Default()
+	defaultSubscriber.RegisterBeforeStart(
+		subscriber.SqsQueuePurger(consulChangeQueue),
+	)
+	defaultSubscriber.RegisterListeners(
+		subscriber.SqsMsgListener(consulChangeQueue, defaultHandler.ChangeConsulNodes, &sqs.ReceiveMessageInput{
+			MaxNumberOfMessages: aws.Int64(10),
+			WaitTimeSeconds:     aws.Int64(2),
+		}),
 	)
 
-	_ = clubproto.RegisterClubAdminHandler(service.Server(), rpcHandler)
-	_ = clubproto.RegisterClubStudentHandler(service.Server(), rpcHandler)
-	_ = clubproto.RegisterClubLeaderHandler(service.Server(), rpcHandler)
+	service.Init(
+		micro.BeforeStart(consulAgent.ChangeAllServiceNodes),
+		micro.AfterStart(consulAgent.ChangeAllServiceNodes),
+		micro.AfterStart(defaultSubscriber.StartListening),
+		micro.AfterStart(consulAgent.ServiceNodeRegistry(service.Server())),
+		micro.BeforeStop(consulAgent.ServiceNodeDeregistry(service.Server())),
+	)
+
+	_ = clubproto.RegisterClubAdminHandler(service.Server(), defaultHandler)
+	_ = clubproto.RegisterClubStudentHandler(service.Server(), defaultHandler)
+	_ = clubproto.RegisterClubLeaderHandler(service.Server(), defaultHandler)
 
 	// DB Health checker 실행
 	sqlDB, err := dbc.DB()
@@ -147,7 +169,7 @@ func main() {
 		Name:       "DB-Checker",
 		Checker:    dbChecker,
 		Interval:   time.Second * 5,
-		OnComplete: closure.TTLCheckHandlerAboutDB(service.Server(), consul),
+		OnComplete: closure.TTLCheckHandlerAboutDB(service.Server(), consulCli),
 	}
 	if err = h.AddChecks([]*health.Config{dbHealthCfg}); err != nil {
 		log.Fatalf("unable to register health checks, err: %v", err)
@@ -160,17 +182,4 @@ func main() {
 	if err := service.Run(); err != nil {
 		log.Fatalf("error occurs while running service, err: %v", err)
 	}
-}
-
-func getRandomPortNotInUsedWithRange(min, max int) (port int) {
-	for {
-		port = rand.Intn(max - min) + min
-		conn, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
-		if err != nil {
-			continue
-		}
-		_ = conn.Close()
-		break
-	}
-	return
 }
